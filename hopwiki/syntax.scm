5e606ec032163bdd3219a33b8fd6a9355eea7994
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2006-04-04 08:16:38 +0200

    Wiki parser improvements.

--- hopwiki/syntax.scm
+++ hopwiki/syntax.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Mon Apr  3 07:05:06 2006                          */
-;*    Last change :  Mon Apr  3 16:03:39 2006 (serrano)                */
+;*    Last change :  Tue Apr  4 07:52:22 2006 (serrano)                */
 ;*    Copyright   :  2006 Manuel Serrano                               */
 ;*    -------------------------------------------------------------    */
 ;*    The HOP wiki syntax tools                                        */
@@ -33,8 +33,7 @@
 	       (tt::procedure (default <TT>))
 	       (code::procedure (default <CODE>))
 	       (math::procedure (default <PRE>))
-	       (pre::procedure (default <PRE>))
-	       )
+	       (pre::procedure (default <PRE>)))
 	    
 	    (wiki-string->hop ::bstring #!optional syntax)))
 
@@ -78,21 +77,47 @@
       (define (in-state? st)
 	 (and (pair? state) (eq? (vector-ref (car state) 0) st)))
       
-      (define (enter-state! st fun)
-	 (set! state (cons (vector st fun '()) state)))
+      (define (enter-state! st fun value)
+	 (set! state (cons (vector st fun '() value) state)))
       
       (define (exit-state!)
 	 (let ((st (car state)))
 	    (let ((el ((vector-ref st 1) (reverse! (vector-ref st 2)))))
 	       (set! state (cdr state))
 	       el)))
+
+      (define (state-value)
+	 (vector-ref (car state) 3))
       
       (define (abort-states!)
-	 (let ((els (map (lambda (st)
-			    ((vector-ref st 1) (reverse! (vector-ref st 2))))
-			 state)))
-	    (set! state '())
-	    (reverse! els)))
+	 (let loop ((st state)
+		    (el '()))
+	    (if (null? st)
+		(begin
+		   (set! state '())
+		   el)
+		(let* ((s (car st))
+		       (kons (vector-ref s 1))
+		       (els (vector-ref s 2)))
+		   (loop (cdr st) (kons (reverse! (cons el els))))))))
+
+      (define (abort-states-until! stop)
+	 (let loop ((st state)
+		    (el '()))
+	    (if (null? st)
+		(begin
+		   (set! state '())
+		   el)
+		(let* ((s (car st))
+		       (id (vector-ref s 0))
+		       (kons (vector-ref s 1))
+		       (els (vector-ref s 2))
+		       (nel (kons (reverse! (cons el els)))))
+		   (if (eq? id stop)
+		       (begin
+			  (set! state (cdr st))
+			  nel)
+		       (loop (cdr st) nel))))))
 
       (define (add-expr! str)
 	 (if (pair? state)
@@ -104,29 +129,28 @@
       (define (add-result! res)
 	 (set! result (cons res result)))
       
-      (define (add-results! . res)
-	 (set! result (append res result)))
-      
       (define (the-result)
 	 (reverse! result))
       
       ;; paragraph
       ((: "\\\\" (: (? #\Return) #\Newline))
-       (add-results! (abort-states!))
-       (enter-state! 'p (wiki-syntax-p syn))
+       (add-result! (abort-states!))
+       (enter-state! 'p (wiki-syntax-p syn) #f)
        (ignore))
       
       ;; newline
       ((bol (: (? #\Return) #\Newline))
-       (add-results! (abort-states!))
-       (enter-state! 'p (lambda (e)
-			   (if (null? e)
-			       "\n"
-			       ((wiki-syntax-p syn) e))))
+       (add-result! (abort-states!))
+       (enter-state! 'p
+		     (lambda (e)
+			(if (null? e)
+			    "\n"
+			    ((wiki-syntax-p syn) e)))
+		     #f)
        (ignore))
       
       ;; simple text
-      ((+ (out "*=/_$,`'() \\\n"))
+      ((+ (out "*=/_-$,`'() \\\n"))
        (add-expr! (the-string))
        (ignore))
       
@@ -135,7 +159,7 @@
        (let ((s (the-symbol)))
 	  (if (in-state? s)
 	      (begin
-		 (add-result! (exit-state!))
+		 (add-expr! (exit-state!))
 		 (ignore))
 	      (let ((constr (case (the-length)
 			       ((5) (wiki-syntax-h4 syn))
@@ -143,29 +167,52 @@
 			       ((3) (wiki-syntax-h2 syn))
 			       ((2) (wiki-syntax-h1 syn))
 			       (else (wiki-syntax-h5 syn)))))
-		 (add-results! (abort-states!))
-		 (enter-state! s constr)
+		 (add-result! (abort-states!))
+		 (enter-state! s constr #f)
 		 (ignore)))))
       ((>= 2 #\=)
        (let ((s (the-symbol)))
 	  (if (in-state? s)
 	      (begin
-		 (add-result! (exit-state!))
+		 (add-expr! (exit-state!))
 		 (ignore))
 	      ;; an error
 	      (ignore))))
 
       ;; verbatim mode
-      ((bol (: "  " (+ all)))
+      ((bol (: "  " (* (in " \t")) (? (: (out "*- \t") (* all)))))
        (if (in-state? 'pre)
 	   (begin
 	      (add-expr! (the-substring 2 (the-length)))
 	      (ignore))
 	   (begin
-	      (add-results! (abort-states!))
-	      (enter-state! 'pre (wiki-syntax-pre syn))
+	      (add-result! (abort-states!))
+	      (enter-state! 'pre (wiki-syntax-pre syn) #f)
 	      (add-expr! (the-substring 2 (the-length)))
 	      (ignore))))
+
+      ;; itemize/enumerate
+      ((bol (: "  " (* " ") (in "*-")))
+       (let* ((s (the-substring (-fx (the-length) 1) (the-length)))
+	      (c (string-ref s 0))
+	      (st (if (char=? c #\*) 'ul 'ol))
+	      (val (the-length)))
+	  (let loop ()
+	     (cond
+		((or (not (in-state? 'li)) (>fx val (state-value)))
+		 (if (eq? st 'ul)
+		     (enter-state! st (wiki-syntax-ul syn) #f)
+		     (enter-state! st (wiki-syntax-ol syn) #f))
+		 (enter-state! 'li (wiki-syntax-li syn) val)
+		 (ignore))
+		((<fx val (state-value))
+		 (add-expr! (exit-state!))
+		 (add-expr! (exit-state!))
+		 (loop))
+		(else
+		 (add-expr! (exit-state!))
+		 (enter-state! 'li (wiki-syntax-li syn) val)
+		 (ignore))))))
       
       ;; standard markups
       ("**"
@@ -174,7 +221,7 @@
 	      (add-expr! (exit-state!))
 	      (ignore))
 	   (begin
-	      (enter-state! '** (wiki-syntax-b syn))
+	      (enter-state! '** (wiki-syntax-b syn) #f)
 	      (ignore))))
       ("//"
        (if (in-state? '//)
@@ -182,7 +229,7 @@
 	      (add-expr! (exit-state!))
 	      (ignore))
 	   (begin
-	      (enter-state! '// (wiki-syntax-em syn))
+	      (enter-state! '// (wiki-syntax-em syn) #f)
 	      (ignore))))
       ("__"
        (if (in-state? '__)
@@ -190,7 +237,15 @@
 	      (add-expr! (exit-state!))
 	      (ignore))
 	   (begin
-	      (enter-state! '__ (wiki-syntax-u syn))
+	      (enter-state! '__ (wiki-syntax-u syn) #f)
+	      (ignore))))
+      ("--"
+       (if (in-state? '--)
+	   (begin
+	      (add-expr! (exit-state!))
+	      (ignore))
+	   (begin
+	      (enter-state! '-- (wiki-syntax-del syn) #f)
 	      (ignore))))
       ("$$"
        (if (in-state? '$$)
@@ -198,30 +253,20 @@
 	      (add-expr! (exit-state!))
 	      (ignore))
 	   (begin
-	      (enter-state! '$$ (wiki-syntax-math syn))
+	      (enter-state! '$$ (wiki-syntax-math syn) #f)
 	      (ignore))))
       ("``"
-       (enter-state! 'tt (wiki-syntax-tt syn))
+       (enter-state! 'tt (wiki-syntax-tt syn) #f)
        (ignore))
       ("''"
-       (if (in-state? 'tt)
-	   (begin
-	      (add-expr! (exit-state!))
-	      (ignore))
-	   (begin
-	      (add-result! (exit-state!))
-	      (ignore))))
+       (add-expr! (abort-states-until! 'tt))
+       (ignore))
       ("(("
-       (enter-state! 'code (wiki-syntax-code syn))
+       (enter-state! 'code (wiki-syntax-code syn) #f)
        (ignore))
       ("))"
-       (if (in-state? 'code)
-	   (begin
-	      (add-expr! (exit-state!))
-	      (ignore))
-	   (begin
-	      (add-result! (exit-state!))
-	      (ignore))))
+       (add-expr! (abort-states-until! 'code))
+       (ignore))
 
       ;; embedded hop
       (",("
@@ -233,7 +278,7 @@
        (ignore))
        
       ;; single escape characters
-      ((in "*=/_$,`'() \\\n")
+      ((in "*=/_-$,`'() \\\n")
        (add-expr! (the-string))
        (ignore))
 
@@ -241,7 +286,7 @@
        (let ((c (the-failure)))
 	  (if (eof-object? c)
 	      (begin
-		 (add-results! (abort-states!))
+		 (add-result! (abort-states!))
 		 (the-result))
 	      (wiki-read-error "Illegal character" (string c) (the-port)))))))
 
