121b2c79661127855e6bfd3b6b90f1d8951a31ca
Erick Gallesio <eg@essi.fr>
2006-05-07 22:58:50 +0200

    New version of HTML editor

--- runtime/html-edit.scm
+++ runtime/html-edit.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Erick Gallesio                                    */
 ;*    Creation    :  Sat Apr  8 13:15:13 2006                          */
-;*    Last change :  Mon Apr 10 22:59:15 2006 (eg)                     */
+;*    Last change :  Sun May  7 22:57:08 2006 (eg)                     */
 ;*    -------------------------------------------------------------    */
 ;*    The HOP implementation of <EDITOR>.                              */
 ;*=====================================================================*/
@@ -17,82 +17,157 @@
 	    "xml.sch")
 
    (import  __hop_param
+	    __hop_configure
 	    __hop_xml)
 
    (export  (<EDITOR> . ::obj)))
 
 
-(define *icons-dir* "/mnt/users/eg/Trash/editor-icons")
+(define *icons-dir*  (make-file-path (hop-share-directory) "editor"))
+(define *popups-dir* *icons-dir*)
+
+(define *styles* '((" --style--" "")
+		   ("Title 1" "h1") ("Title 2" "h2") ("Title 3" "h3")
+		   ("Title 4" "h4") ("Title 5" "h5") ("Title 6" "h6")
+		   ("Address" "address") ("Paragraph" "p")))
+
+(define *fonts* '(" --font--"
+		  "Serif" "Sans Serif" "Arial" "Tahoma" "Verdana" "Courier New"
+		  "Georgia" "Times New Roman" "Impact" "Comic Sans MS"))
 
 
-(define (make-toolbar cls width)
+(define (make-palette id)
+  ;; Web safe palette (a variant of the 216 colors palette reduced to 27 ..)
+  (let Loop ((r #x33) (g #x33) (b #x33)
+	     (res '()))
+    (cond
+      ((> b #xff) (Loop r (+ g #x66) #x33 res))
+      ((> g #xff) (Loop (+ r #x66) #x33 #x33 res))
+      ((> r #xff) (reverse res))
+      (else
+       (let ((col (format "rgb(~a,~a,~a)" r g b)))
+	 (Loop r g (+ b #x66)
+	       (cons
+		(<DIV> :style (string-append
+			         "width:10px;height:10px;"
+				 "background:" col ";"
+				 "border:1px inset gray;margin:1px;float:left;")
+		       :onclick (format "hop_edit_colorize(~s,~s)" id col))
+	      res)))))))
+
+
+;*---------------------------------------------------------------------*/
+;*    make-toolbar ...                                                 */
+;*---------------------------------------------------------------------*/
+(define (make-toolbar id width)
   (define (<SPACER>)
     (<TD> :width "6px"))
 
   (define (<SEPARATOR>)
     (<TD> :width "12px"  :align "center"
-      (<IMG> :src (make-file-path *icons-dir* "separator.gif") :border 0)))
+      (<IMG> :src (make-file-path *icons-dir* "ed-separator.png") :border 0)))
 
   (define (<FILER>)
     (<TD> "&nbsp;"))
   
   (define (<ICON> name tooltip image)
-    (let ((icon1 (make-file-path *icons-dir* (string-append image ".gif")))
-	  (icon2 (make-file-path *icons-dir* (string-append image "_on.gif")))
-	  (cls1  "hop-edit-button")
-	  (cls2  "hop-edit-button-over"))
+    (let ((icon (make-file-path *icons-dir* (string-append image ".png"))))
       (<TD> :style "width: 22px"
-	(<IMG> :src icon1 :class cls1 :border 0 :width "20px" :height "20px"
-	       :title tooltip
-	       :onmouseover (format "this.src= ~s; this.className=~s" icon2 cls2)
-	       :onmouseout  (format "this.src= ~s; this.className=~s" icon1 cls1)))))
+	(<IMG> :src icon :class "hop-edit-button" :id (format "~a-but-~a" id name)
+	       :border 0 :style "width:16px;height:16px" :title tooltip
+	       :onclick (format "hop_edit_action(~s, ~s)" id name)
+	       :onmouseover "if (!hop_edit_src) className='hop-edit-button-over'"
+	       :onmouseout  "if (!hop_edit_src) className='hop-edit-button'"))))
 
-  (let ((bg (make-file-path *icons-dir* "background_silver.jpg")))
+  
+  (define (<STYLES>)
+    (<TD> :width 50
+	  (<SELECT> :id (string-append "hop-edit-styles-" id)
+	   :style "height: 20px; background: #ddf; border: 1px solid #aaa"
+	   (map (lambda (x)
+		  (<OPTION> :onclick (format "hop_edit_style_set(~s,~s)"
+					     id (cadr x))
+			    (car x)))
+		*styles*))))
+
+  (define (<FONTS>)
+    (<TD> :width 65
+	  (<SELECT> :id (string-append "hop-edit-fonts-" id)
+	   :style "height: 20px; background: #ddf; border: 1px solid #aaa"
+	   (map (lambda (x)
+		  (<OPTION> :onclick (format "hop_edit_font_set(~s,~s)" id x) x))
+		*fonts*))))
+
+  (define (<FONT-SIZES>)
+    (<TD> :width 20
+	  (<SELECT> :id (string-append "hop-edit-fs-" id)
+	   :style "height: 20px; background: #ddf; border: 1px solid #aaa"
+	   (map (lambda (x)
+		  (<OPTION> :onclick (format "hop_edit_fontsize_set(~s,~s)" id x) x))
+		'(1 2 3 4 5 6 7)))))
+
+  (define (<PALETTE>)
+    (let ((palette (string-append id "-palette")))
+      (<DIV> :style (string-append "display:none;position:absolute;"
+				   "border: 2px ridge #555; padding:4px;"
+				   "background:#eee;cursor:pointer;width:100;")
+	     :id palette
+	     :onclick "this.style.display = 'none'"
+	     (<DIV> (make-palette id)))))
+  
+  (let ((bg (make-file-path *icons-dir* "ed-silver-bg.png")))
     (<DIV>
-     (<TABLE> :cellpadding 0 :cellspacing 0 :border 0 :class "hop-edit-toolbars"
-	      :width width
+     ;; The (not yet visible) color palette
+     (<PALETTE>)
+     ;; First line of the toolbar
+     (<TABLE> :cellpadding 0 :cellspacing 0 :class "hop-edit-toolbars"
+	      :width width :style "border-bottom: 0px"
 	 (<TR> :style (format "height: 26px; background-image: url(~s);" bg)
 	  (<SPACER>)
-	  (<ICON> "bold" "Bold" "bold")
-	  (<ICON> "italic" "Italic" "italic")
-	  (<ICON> "underline" "Underline" "underline")
-	  (<ICON> "strikethrough" "Strike Through" "strikethrough")
+	  (<ICON> "submit" "Submit" "ed-page-go")
+	  (<ICON> "cancel" "Cancel" "ed-page")
 	  (<SEPARATOR>)
-	  (<ICON> "subscript" "Subscript" "subscript")
-	  (<ICON> "superscript" "Superscript" "superscript")
+	  (<ICON> "bold" "Bold" "ed-bold")
+	  (<ICON> "italic" "Italic" "ed-italic")
+	  (<ICON> "underline" "Underline" "ed-underline")
+	  (<ICON> "strikethrough" "Strike Through" "ed-strikethrough")
 	  (<SEPARATOR>)
-	  (<ICON> "justifyleft"   "Left alignment" 	"justify_left")
-	  (<ICON> "justifycenter" "Center alignment" 	"justify_center")
-	  (<ICON> "justifyright"  "Right alignment"	"justify_right")
-	  (<ICON> "justify"       "Justified"		"justify")
+	  (<ICON> "subscript" "Subscript" "ed-subscript")
+	  (<ICON> "superscript" "Superscript" "ed-superscript")
 	  (<SEPARATOR>)
-	  (<ICON> "unorderedlist" "Bullet" "list_unordered")
-	  (<ICON> "orderedlist" "Numbering" "list_ordered")
+	  (<ICON> "justifyleft"   "Left alignment" 	"ed-justify-left")
+	  (<ICON> "justifycenter" "Center alignment" 	"ed-justify-center")
+	  (<ICON> "justifyright"  "Right alignment"	"ed-justify-right")
+	  (<ICON> "justifyfull"   "Justified"		"ed-justify")
 	  (<SEPARATOR>)
-	  (<ICON> "indent" "Increase indent" "indent_left")
-	  (<ICON> "outdent" "Decrease indent" "indent_right")
-	  (<FILER>))
+	  (<ICON> "insertunorderedlist" "Bullet" "ed-list-unordered")
+	  (<ICON> "insertorderedlist" "Numbering" "ed-list-ordered")
+	  (<SEPARATOR>)
+	  (<ICON> "undo" "Undo" "ed-undo")
+	  (<ICON> "redo" "Redo" "ed-redo")
+	  (<SEPARATOR>)
+	  (<ICON> "indent" "Increase indent" "ed-indent")
+	  (<ICON> "outdent" "Decrease indent" "ed-unindent")
+	  (<FILER>)))
+    
+     ;; Second line of the toolbar
+     (<TABLE> :cellpadding 0 :cellspacing 0 :class "hop-edit-toolbars"
+	      :width width 
 	 (<TR> :style (format "height: 26px; background-image: url(~s);" bg)
 	  (<SPACER>)
-	  (<ICON> "forecolor" "Foreground" "forecolor")
-	  (<ICON> "backcolor" "Background" "backcolor")
+          (<ICON> "viewsource"  "Alternate view" "ed-html")
 	  (<SEPARATOR>)
-	  (<ICON> "cut" "Cut" "cut")
-	  (<ICON> "copy" "Copy" "copy")
-	  (<ICON> "paste" "Paste" "paste")
+	  (<STYLES>) (<SPACER>)
+	  (<FONTS>) (<SPACER>)
+	  (<FONT-SIZES>) (<SPACER>)
+	  (<ICON> "forecolor" "Foreground" "ed-foreground")
+	  (<ICON> "hilitecolor" "Background" "ed-background")
 	  (<SEPARATOR>)
-	  (<ICON> "undo" "Undo" "undo")
-	  (<ICON> "redo" "Redo" "redo")
-	  (<SEPARATOR>)
-	  (<ICON> "inserttable" "Insert table" "insert_table")
-	  (<ICON> "insertimage" "Insert image" "insert_picture")
-	  (<ICON> "insertlink"  "Insert link" "insert_hyperlink")
-	  (<SEPARATOR>)
-	  (<ICON> "viewsource"  "Alternate view" "view_source")
-	  (<SEPARATOR>)
-	  (<ICON> "help" "Help" "help")
-	  (<FILER>)  ;; Why do we need 2 of them here?
+	  (<ICON> "insertlink"  "Insert link" "ed-link")
+	  (<ICON> "insertimage" "Insert image" "ed-image")
+	  (<ICON> "inserttable" "Insert table" "ed-table")
 	  (<FILER>))))))
+     
 
 ;*---------------------------------------------------------------------*/
 ;*    <EDITOR> ...                                                     */
@@ -102,7 +177,20 @@
 			       (width "100%")
 			       (height "600px")
 			       (name "")
+			       (submit 'hop_edit_submit)
+			       (cancel 'hop_edit_cancel)
 			       body)
-  (<DIV> :style "height:300; background: Ivory"
-     (make-toolbar class width)))
-		       
+  (let ((id (xml-make-id id 'EDITOR)))
+    (<DIV> :style (format "width: ~a" width)
+	   :class "hop-editor"
+       ;; Create the toolbar
+       (make-toolbar id width)
+       ;; Create the (invisible) text area which can be used for forms
+       (<TEXTAREA> :style (format "display:none; width: ~a; height: ~a; border:0;"
+				  width height)
+		   :id id :name name body)
+       ;; Create an iframe for rich text editing
+       (<IFRAME> :id (string-append "hop-edit" id) :frameborder 0
+		 :width width :height height)
+       (<SCRIPT> (format "hop_edit_init(~s, ~s, ~s, ~s)" id *popups-dir*
+			 submit cancel)))))
--- share/hop-edit.js
+++ share/hop-edit.js
@@ -3,8 +3,310 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Erick Gallesio                                    */
 /*    Creation    :  Mon Apr 10 11:43:00 2006                          */
-/*    Last change :  Mon Apr 10 11:44:40 2006 (eg)                     */
+/*    Last change :  Sun May  7 22:41:39 2006 (eg)                     */
 /*    -------------------------------------------------------------    */
 /*    <EDITOR> JavaScript support                                      */
 /*=====================================================================*/
 
+var hop_edit_popups_dir;
+var hop_edit_src;
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_init ...                                                */
+/*---------------------------------------------------------------------*/
+function hop_edit_init(id, popups_dir, submit, cancel)
+{
+    var txt    = document.getElementById(id);
+    var iframe = document.getElementById("hop-edit" + id);
+    var doc    = iframe.contentWindow.document;
+
+    hop_edit_src = false;
+
+    // Copy the textarea content into the iframe
+    doc.open()
+    doc.write(txt.value);
+    doc.close();
+
+    // Store the submit/cancel function 
+    iframe.hop_edit_submit = submit;
+    iframe.hop_edit_cancel = cancel;
+
+    // Make the iframe editable 
+    doc.designMode = "on";
+    
+    // Save the popups directory
+    hop_edit_popups_dir = popups_dir;
+
+    // Install a listener fo KeyPress events
+    iframe.contentDocument.addEventListener("keypress", 
+					    function(e) {
+						hop_edit_keypress_hdlr(e, id);
+					    },
+					    true);
+    // Install an evnt handler to copy back iframe content to textarea if submitting
+    for (var i=0; i < document.forms.length; i++) {
+    	document.forms[i].addEventListener("submit",
+					   function (e) { 
+					       hop_edit_update_textarea(id); 
+					   },
+					   true);
+    }
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_keypress_hdlr ...                                       */
+/*---------------------------------------------------------------------*/
+function hop_edit_keypress_hdlr(e, id)
+{
+    if (e.ctrlKey) {
+	var propagate=false,
+	    char=e.charCode;
+	switch (char) {
+	  case 98:  hop_edit_action(id, "bold"); break;
+	  case 105: hop_edit_action(id, "italic"); break;
+	  case 117: hop_edit_action(id, "underline"); break;
+	  default: propagate = true;
+	}
+	if (!propagate) {
+	    e.preventDefault();
+	    e.stopPropagation();
+	}
+    }
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_action ...                                              */
+/*---------------------------------------------------------------------*/
+function hop_edit_action(id, action, value)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+
+    if (action == "viewsource")
+	hop_edit_view_source(id);
+    else {
+	if (hop_edit_src) return;
+
+	if (action == "insertlink")
+	    hop_edit_insert_link(id);
+	else if (action == "insertimage")
+	    hop_edit_insert_image(id);
+	else if (action == "inserttable") 
+	    alert("TODO");
+	else if (action == "forecolor" || action == "hilitecolor")
+	    hop_edit_palette(id, action);
+	else if (action == "submit") {
+	    hop_edit_update_textarea(id); 
+	    (eval(iframe.hop_edit_submit)) ( id );
+	} else  if (action == "cancel") {
+	    (eval(iframe.hop_edit_cancel)) ( id );
+	} else
+	    iframe.contentDocument.execCommand(action, false, value);
+    }
+}
+
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_style_set ...                                           */
+/*---------------------------------------------------------------------*/
+function hop_edit_style_set(id, value)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    
+    iframe.contentDocument.execCommand("formatblock", false, '<'+value+'>');
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_font_set ...                                            */
+/*---------------------------------------------------------------------*/
+function hop_edit_font_set(id, value)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    var action = (value == " --font--") ? "removeformat" : "fontname";
+
+    iframe.contentDocument.execCommand(action, false, value);
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_fontsize_set ...                                            */
+/*---------------------------------------------------------------------*/
+function hop_edit_fontsize_set(id, value)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+
+    iframe.contentDocument.execCommand("fontsize", false, value);
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_open_window ...                                         */
+/*---------------------------------------------------------------------*/
+function hop_edit_open_window(url, name, width, height)
+{
+    window.open(url, name, "location=0,status=0,scrollbars=0,width=" + 
+		width + ',height=' + height);
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_insert_link ...                                         */
+/*---------------------------------------------------------------------*/
+function hop_edit_insert_link(id) 
+{
+    var iframe = document.getElementById("hop-edit" + id);
+
+    if (iframe.contentDocument.getSelection() == "")
+	alert("Selection is not available");
+    else {
+	document.hop_edit_id_link=id; // Retain the id which opens the window
+	hop_edit_open_window(hop_edit_popups_dir + "/link.hop", 
+			     "hop_edit_link", 400, 200);
+    }
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_accept_link ...                                         */
+/*---------------------------------------------------------------------*/
+function hop_edit_accept_link(url)
+{
+    var value  = document.getElementById("hop-edit-url").value;
+    var id     = window.opener.document.hop_edit_id_link;
+    var iframe = window.opener.document.getElementById("hop-edit" + id);
+    var cmd    = (value == "") ? "unlink" : "createlink";
+
+    iframe.contentDocument.execCommand(cmd, false, value);
+    window.close();
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_insert_image ...                                        */
+/*---------------------------------------------------------------------*/
+function hop_edit_insert_image(id) 
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    document.hop_edit_id_image=id; // Retain the id which opens the window
+    hop_edit_open_window(hop_edit_popups_dir + "/image.hop", 
+			 "hop_edit_image", 400, 200);
+}
+
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_accept_image ...                                        */
+/*---------------------------------------------------------------------*/
+function hop_edit_accept_image(url)
+{
+    var value  = document.getElementById("hop-edit-url").value;
+    var id     = window.opener.document.hop_edit_id_image;
+    var iframe = window.opener.document.getElementById("hop-edit" + id);
+
+    iframe.contentDocument.execCommand("insertimage", false, value);
+    window.close();
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_palette ...                                             */
+/*---------------------------------------------------------------------*/
+function hop_edit_palette(id, action)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    var pal    = document.getElementById(id + "-palette");
+    var but    = document.getElementById(id + "-but-" + action);
+
+    pal.style.display = "block";
+    pal.style.left = hop_element_x(but) - 20;
+    pal.style.top  = hop_element_y(but) + but.height ;
+    // Retain the action to do
+    iframe.hop_colorize_action = action;
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_colorize ...                                            */
+/*---------------------------------------------------------------------*/
+function hop_edit_colorize(id, color)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    var action = iframe.hop_colorize_action;
+    
+    iframe.contentDocument.execCommand(action, false, color);
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_submit/cancel ...                                       */
+/*---------------------------------------------------------------------*/
+function hop_edit_submit_cancel(id, abort)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    
+    for (el = iframe; el ; el = el.parentNode) {
+	if (el instanceof HTMLFormElement) { 
+	    //	var but = document.createElement("button"); 
+	    //	but.value = value;
+	    //	but.name = "abort";
+	    //	but.style.display = "none"; 
+	    //	el.appendChild(but);
+	    //	but.click();
+	    if (abort)
+		iframe.contentDocument.body.innerHTML = "";
+	    el.submit();
+	    break;
+	} 
+    }
+}
+
+function hop_edit_submit(id) { hop_edit_submit_cancel(id, false); }
+
+function hop_edit_cancel(id) { hop_edit_submit_cancel(id, true); }
+
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_update_textarea ...                                     */
+/*---------------------------------------------------------------------*/
+function hop_edit_update_textarea(id)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    var txt    = document.getElementById(id);
+
+    txt.value = iframe.contentDocument.body.innerHTML;
+}
+
+/*---------------------------------------------------------------------*/
+/*    hop_edit_view_source ...                                         */
+/*---------------------------------------------------------------------*/
+function hop_edit_view_source(id)
+{
+    var iframe = document.getElementById("hop-edit" + id);
+    var txt    = document.getElementById(id);
+    var but    = document.getElementById(id + "-but-viewsource");
+    var butAct = but.onclick;
+    var butSrc = but.src;
+
+    // Swap Iframe and Textarea
+    iframe.style.display = "none";
+    txt.value = iframe.contentDocument.body.innerHTML;
+    txt.style.display = "block";
+
+    // Make select boxes and buttons inative
+    var styles = document.getElementById("hop-edit-styles-" + id);
+    var fonts  = document.getElementById("hop-edit-fonts-" + id);
+    var fsizes = document.getElementById("hop-edit-fs-" + id);
+
+    styles.disabled  = true;
+    fonts.disabled   = true;
+    fsizes.disabled  = true;
+    hop_edit_src     = true;	// sufficient for all the buttons
+
+    // Change the button for restauring text
+    but.src = hop_edit_popups_dir + "/ed-nohtml.png";
+    
+    // Change the binding of the view source button
+    but.onclick = function(event) {
+	iframe.contentDocument.body.innerHTML = txt.value;
+	txt.style.display = "none";
+	iframe.style.display = "block";
+
+	but.onclick      = butAct;
+	but.src          = butSrc;
+
+	styles.disabled  = false;
+	fonts.disabled   = false;
+	fsizes.disabled  = false;
+	hop_edit_src     = false;
+    }
+}
