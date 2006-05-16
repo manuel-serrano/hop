db11abd189911f0379603b859129cacefa3adb5b
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2006-05-16 09:55:58 +0200

    .

--- share/hop.scm
+++ share/hop.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Mon Jan 17 13:55:11 2005                          */
-;*    Last change :  Thu Feb  2 16:24:55 2006 (serrano)                */
+;*    Last change :  Tue May 16 09:55:51 2006 (serrano)                */
 ;*    Copyright   :  2005-06 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Hop initialization (default filtering).                          */
@@ -18,10 +18,10 @@
 ;*---------------------------------------------------------------------*/
 ;*    autoloads                                                        */
 ;*---------------------------------------------------------------------*/
-;; dired
-(autoload "dired/dired.scm"
-	  (lambda (req)
-	     (with-access::http-request req (path localhostp)
-		(and localhostp
-		     (or (and (file-exists? path) (directory? path))
-			 ((autoload-prefix "/hop/dired") req))))))
+;* ;; dired                                                            */
+;* (autoload "dired/dired.scm"                                         */
+;* 	  (lambda (req)                                                */
+;* 	     (with-access::http-request req (path localhostp)          */
+;* 		(and localhostp                                        */
+;* 		     (or (and (file-exists? path) (directory? path))   */
+;* 			 ((autoload-prefix "/hop/dired") req))))))     */
