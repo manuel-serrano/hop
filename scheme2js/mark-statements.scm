b080e3c6e5b853857774814a310ca0b5d9423c9d
Florian Loitsch <florian.loitsch@sophia.inria.fr>
2006-05-17 11:00:49 +0200

    new optimization (while), and refactoring (mark-statement).

---
+++ scheme2js/mark-statements.scm
@@ -0,0 +1,156 @@
+(module mark-statements
+   (include "protobject.sch")
+   (include "nodes.sch")
+   (option (loadq "protobject-eval.sch"))
+   (import protobject
+	   nodes
+	   var
+	   verbose)
+   (export (mark-statements tree::pobject)
+	   (statement-form? o::pobject)
+	   (marked-node? o::pobject)
+	   (mark-node! o::pobject statement-form?)
+	   (mark-statement-form! o::pobject statement-form?)))
+
+(define (mark-statements tree::pobject)
+   (verbose "  mark-statements")
+   (overload traverse mark-statements (Node
+				       Part
+				       Lambda
+				       If
+				       Case
+				       Begin
+				       Bind-exit
+				       With-handler
+				       Call
+				       Tail-rec
+				       While
+				       Tail-rec-call
+				       Return
+				       Closure-alloc
+				       Label
+				       Break
+				       Set!)
+	     (set! (node 'Node).proto.default-traverse-value #f)
+	     (tree.traverse)
+	     (delete! (node 'Node).proto.default-traverse-value)))
+
+(define (mark-statement-form! o statement-form?)
+   (mark-node! o statement-form?))
+
+(define (mark-node! o statement-form?)
+   (if statement-form?
+       (set! o.statement-form? #t)
+       (delete! o.statement-form?)))
+
+(define (statement-form? o::pobject)
+   (marked-node? o))
+
+(define (marked-node? o)
+   o.statement-form?)
+
+(define (list-mark-statements l)
+   (let loop ((l l)
+	      (res #f))
+      (if (null? l)
+	  res
+	  (loop (cdr l)
+		(if ((car l).traverse)
+		    #t
+		    res)))))
+
+(define-pmethod (Node-mark-statements)
+   (let ((res (this.traverse0)))
+      (mark-node! this res)
+      res))
+
+(define-pmethod (Part-mark-statements)
+   (let* ((body-res (this.body.traverse))
+	  (res (or body-res this.prefer-statement-form?)))
+      (mark-node! this res)
+      res))
+
+(define-pmethod (Lambda-mark-statements)
+   (this.body.traverse)
+   (mark-node! this #f)
+   #f)
+
+(define-pmethod (If-mark-statements)
+   (let* ((test-res (this.test.traverse))
+	  (then-res (this.then.traverse))
+	  (else-res (this.else.traverse))
+	  (res (or test-res then-res else-res)))
+      (mark-node! this res)
+      res))
+
+(define-pmethod (Case-mark-statements)
+   (this.key.traverse)
+   (map (lambda (node) (node.traverse)) this.clauses)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Begin-mark-statements)
+   (let ((res (list-mark-statements this.exprs)))
+      (mark-node! this res)
+      res))
+
+(define-pmethod (Bind-exit-mark-statements)
+   ;; don't need to go into escape and result-vars.
+   (this.body.traverse)
+   (this.invoc-body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (With-handler-mark-statements)
+   ;; don't need to go into exception-var
+   (this.catch.traverse)
+   (this.body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Call-mark-statements)
+   (let* ((operands-tmp (list-mark-statements this.operands))
+	  (operator-tmp (this.operator.traverse))
+	  (res (or operands-tmp operator-tmp)))
+      (mark-node! this res)
+      res))
+
+(define-pmethod (Tail-rec-mark-statements)
+   (this.body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (While-mark-statements)
+   (this.test.traverse)
+   (this.body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Tail-rec-call-mark-statements)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Return-mark-statements)
+   (this.val.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Closure-alloc-mark-statements)
+   (this.body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Label-mark-statements)
+   (this.body.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Break-mark-statements)
+   (this.val.traverse)
+   (mark-node! this #t)
+   #t)
+
+(define-pmethod (Set!-mark-statements)
+   (let ((res (this.val.traverse)))
+      (mark-node! this res)
+      res))
---
+++ scheme2js/while.scm
@@ -0,0 +1,203 @@
+(module while
+   (include "protobject.sch")
+   (include "nodes.sch")
+   (include "tools.sch")
+   (option (loadq "protobject-eval.sch"))
+   (import protobject
+	   mark-statements
+	   config
+	   nodes
+	   var
+	   tail
+	   verbose)
+   (export (while! tree::pobject)))
+
+(define (while! tree)
+   (when (config 'while)
+      (verbose "while")
+      (gather-info tree)
+      (mark-statements tree)
+      (apply-patterns! tree)
+      (finish-transformation! tree)
+      ))
+
+(define (gather-info tree)
+   (overload traverse gather (Node
+			      Scope
+			      Closure-alloc
+			      If
+			      Case
+			      Tail-rec
+			      Set!
+			      Begin
+			      Bind-exit
+			      With-handler
+			      Call
+			      Tail-rec-call)
+	     (set! (node 'Node).proto.default-traverse-value '())
+	     (tree.traverse #f)
+	     (delete! (node 'Node).proto.default-traverse-value)))
+
+(define (union . Lls)
+   (let ((ht (make-eq-hashtable)))
+      (for-each (lambda (l)
+		   (for-each (lambda (x)
+				(hashtable-put! ht x #t))
+			     l))
+		Lls)
+      (hashtable-key-list ht)))
+
+(define-pmethod (Node-gather surrounding-tail-rec)
+   (this.traverse1 surrounding-tail-rec))
+
+(define-pmethod (Scope-gather surrounding-tail-rec)
+   (this.traverse1 #f)
+   '())
+
+(define-pmethod (Closure-alloc-gather surrounding-tail-rec)
+   ;; surrounding-tail-rec must be a node, otherwise we screwed up.
+   [assert (surrounding-tail-rec) surrounding-tail-rec]
+   (set! surrounding-tail-rec.contains-closure-allocs? #t)
+   (this.traverse1 surrounding-tail-rec))
+
+(define-pmethod (If-gather surrounding-tail-rec)
+   (let ((test-tr-calls (this.test.traverse surrounding-tail-rec))
+	 (then-tr-calls (this.then.traverse surrounding-tail-rec))
+	 (else-tr-calls (this.else.traverse surrounding-tail-rec)))
+      (set! this.test-tail-rec-calls test-tr-calls)
+      (set! this.then-tail-rec-calls then-tr-calls)
+      (set! this.else-tail-rec-calls else-tr-calls)
+      (union test-tr-calls then-tr-calls else-tr-calls)))
+
+(define-pmethod (Case-gather surrounding-tail-rec)
+   (apply union (map (lambda (n) (n.traverse surrounding-tail-rec))
+		     this.clauses)))
+
+(define-pmethod (Tail-rec-gather surrounding-tail-rec)
+   (this.traverse1 this))
+
+(define-pmethod (Set!-gather surrounding-tail-rec)
+   ;; don't need to visit the lvalue
+   (this.val.traverse surrounding-tail-rec))
+
+(define-pmethod (Begin-gather surrounding-tail-rec)
+   (apply union (map (lambda (n) (n.traverse surrounding-tail-rec))
+		     this.exprs)))
+
+(define-pmethod (Bind-exit-gather surrounding-tail-rec)
+   ;; don't need to visit the escape- and result-var
+   (let ((invoc-body-tr-calls
+	  (this.invoc-body.traverse surrounding-tail-rec)))
+      [assert (invoc-body-tr-calls) (null? invoc-body-tr-calls)])
+   (this.body.traverse surrounding-tail-rec))
+
+(define-pmethod (With-handler-gather surrounding-tail-rec)
+   ;; don't need to visit the exception-var
+   (let ((catch-tr-calls (this.catch.traverse surrounding-tail-rec))
+	 (body-tr-calls (this.body.traverse surrounding-tail-rec)))
+      [assert (catch-tr-calls) (null? catch-tr-calls)]
+      body-tr-calls))
+
+(define-pmethod (Call-gather surrounding-tail-rec)
+   (apply union (map (lambda (n) (n.traverse surrounding-tail-rec))
+		     (cons this.operator this.operands))))
+
+(define-pmethod (Tail-rec-call-gather surrounding-tail-rec)
+   (list this.label))
+
+(define (apply-patterns! tree)
+   (define *id->js-var* #unspecified)
+
+   (define-pmethod (Node-apply-patterns!)
+      (this.traverse0!))
+
+   (define-pmethod (Program-apply-patterns!)
+      (set! *id->js-var* this.id->js-var)
+      (this.traverse0!))
+
+   (define-pmethod (Tail-rec-apply-patterns!)
+      (define (apply-while-pattern! body tail-rec-label then-continue?)
+	 (let* ((break-label (gensym 'break))
+		(continue-label-node (new-node Label
+					       (if then-continue?
+						   body.then
+						   body.else)
+					       tail-rec-label))
+		(while (new-node While
+				 (if then-continue?
+				     body.test
+				     (new-node Call
+					       ((*id->js-var* 'not).reference)
+					       (list body.test)))
+				 continue-label-node))
+		(bnode (new-node Begin
+				 (list while (if then-continue?
+						 body.else
+						 body.then))))
+		(break-label-node (new-node Label bnode break-label)))
+	    (set! while.break-label break-label)
+	    (set! while.continue-label this.label)
+	    break-label-node))
+
+      (this.traverse0!)
+      (let ((body this.body)
+	    (label this.label))
+	 (if (and (not this.contains-closure-allocs?)
+		  (inherits-from? body (node 'If))
+		  (not (statement-form? body.test)))
+	     (let* ((test-trs body.test-tail-rec-calls)
+		    (then-trs body.then-tail-rec-calls)
+		    (else-trs body.else-tail-rec-calls)
+		    (test-continue? (memq label test-trs))
+		    (then-continue? (memq label then-trs))
+		    (else-continue? (memq label else-trs)))
+		(cond
+		   ((not (or test-continue? then-continue? else-continue?))
+		    ;; should never happen: the loop is never used
+		    ;; just return the If
+		    body)
+		   ((or (and then-continue? else-continue?)
+			test-continue?)
+		    ;; both branches may recursively call continue
+		    ;; or the test calls continue
+		    ;; we don't know how to optimize this.
+		    this)
+		   ((or then-continue? else-continue?) ;; not both.
+		    ;; then-branch calls continue, but else-branch doesn't.
+		    ;; or else-branch calls continue, but not then-branch.
+		    (apply-while-pattern! body this.label then-continue?))
+		   (else
+		    this)))
+	     this)))
+
+   (overload traverse! apply-patterns! (Node
+					Program
+					Tail-rec)
+	     (tree.traverse!)))
+
+(define (finish-transformation! tree)
+   (tail-exprs tree
+	       #f) ;; intermediate-nodes are not tail
+   (overload traverse! finish! (Node
+				While
+				Tail-rec
+				Tail-rec-call)
+	     (tree.traverse! #f #f)))
+
+(define-pmethod (Node-finish! continue-label break-label)
+   (if (and this.tail?
+	    break-label)
+       (new-node Break (this.traverse2! #f #f) break-label)
+       (this.traverse2! continue-label break-label)))
+
+(define-pmethod (While-finish! continue-label break-label)
+   (this.traverse2! this.continue-label this.break-label))
+
+(define-pmethod (Tail-rec-finish! continue-label break-label)
+   (this.traverse2! #f #f))
+
+(define-pmethod (Tail-rec-call-finish! continue-label break-label)
+   (if (and continue-label
+	    (eq? this.label continue-label))
+       (new-node Const #unspecified)
+       this))
