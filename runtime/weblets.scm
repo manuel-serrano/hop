86b2d6199d55efb26921c12c98872a39fd19ce1f
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2006-05-20 11:21:48 +0200

    /HOP/HOP weblet fixes.

--- runtime/weblets.scm
+++ runtime/weblets.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Erick Gallesio                                    */
 ;*    Creation    :  Sat Jan 28 15:38:06 2006 (eg)                     */
-;*    Last change :  Thu May 11 08:46:40 2006 (serrano)                */
+;*    Last change :  Sat May 20 10:30:12 2006 (serrano)                */
 ;*    Copyright   :  2004-06 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Weblets Management                                               */
@@ -22,16 +22,75 @@
 	   __hop_xml
 	   __hop_service
 	   __hop_misc)
-
    
    (export  (weblets-config-directory)
+	    (find-weblets-in-directory ::string)
 	    (get-weblet-infos ::string ::string)
 	    (get-weblet-config ::string)
 	    (get-weblet-config-value ::string ::symbol ::obj)
-	    (find-weblets ::string)
+	    
 	    (autoload-weblets ::pair-nil)
 
 	    (<WEBLET-ABOUT> . args)))
+
+;*---------------------------------------------------------------------*/
+;*    autoload-weblets ...                                             */
+;*---------------------------------------------------------------------*/
+(define (autoload-weblets dirs)
+   (define (install-autoload-prefix path url)
+      (hop-verb 2 "Setting autoload " path " on " url "\n")
+      (autoload path (autoload-prefix url)))
+   (define (is-service-base? req)
+      (with-access::http-request req (path)
+	 (let ((base (hop-service-base)))
+	    (and (substring-at? base path 0)
+		 (let ((lp (string-length path))
+		       (lb (string-length base)))
+		    (or (=fx lp lb)
+			(and (=fx lp (+fx lb 1))
+			     (char=? (string-ref path (-fx lp 1))
+				     #\/))))))))
+   (define (maybe-autoload x)
+      (let ((url (make-file-name (hop-service-base) (cadr (assoc 'name x))))
+	    (path (cadr (assq 'weblet x)))
+	    (active (cadr (assq 'active x)))
+	    (autopred (assq 'autoload x)))
+	 (when active
+	    (if (pair? autopred)
+		(begin
+		   (hop-verb 2 "Setting autoload " path " on " (cadr autopred)
+			     "\n")
+		   (autoload path (eval (cadr autopred))))
+		(install-autoload-prefix path url)))))
+   (for-each (lambda (dir)
+		(for-each maybe-autoload (find-weblets-in-directory dir)))
+	     dirs))
+
+;*---------------------------------------------------------------------*/
+;*    find-weblets-in-directory ...                                    */
+;*---------------------------------------------------------------------*/
+(define (find-weblets-in-directory dir)
+   
+   (define (get-weblet-details dir name)
+      (let* ((conf (get-weblet-config name))
+	     (infos (get-weblet-infos dir name))
+	     (main (assoc 'main-file infos))
+	     (weblet (make-file-path dir
+				     name
+				     (if main
+					 (cadr main)
+					 (string-append name ".hop")))))
+	 (when (file-exists? weblet)
+	    `((name ,name) (weblet ,weblet) ,@conf ,@infos))))
+   
+   (let Loop ((files (directory->list dir))
+	      (res '()))
+      (if (null? files)
+	  res
+	  (let ((web (get-weblet-details dir (car files))))
+	     (if web
+		 (Loop (cdr files) (cons web res))
+		 (Loop (cdr files) res))))))
 
 ;; ----------------------------------------------------------------------
 ;; 	weblets-config-directory ...
@@ -65,67 +124,6 @@
    (let* ((conf (get-weblet-config name))
 	  (v (assoc key conf)))
       (if v (cadr v) default)))
-
-;; ----------------------------------------------------------------------
-;; 	find-weblets ...
-;; ----------------------------------------------------------------------
-(define (find-weblets dir)
-   (define (get-weblet-details dir name)
-      (let* ((conf (get-weblet-config name))
-	     (infos (get-weblet-infos dir name))
-	     (main (assoc 'main-file infos))
-	     (weblet (make-file-path dir
-				     name
-				     (if main
-					 (cadr main)
-					 (string-append name ".hop")))))
-	 (and (file-exists? weblet)
-	      `((name ,name)
-		(weblet ,weblet)
-		,@conf
-		,@infos))))
-   
-   (let Loop ((files (directory->list dir))
-	      (res '()))
-      (if (null? files)
-	  res
-	  (let ((web (get-weblet-details dir (car files))))
-	     (if web
-		 (Loop (cdr files) (cons web res))
-		 (Loop (cdr files) res))))))
-
-;; ----------------------------------------------------------------------
-;; 	autoload-weblets ...
-;; ----------------------------------------------------------------------
-(define (autoload-weblets dirs)
-   (define (install-autoload-prefix path url)
-      (hop-verb 2 "Setting autoload " path " on " url "\n")
-      (autoload path (autoload-prefix url)))
-   (define (is-service-base? req)
-      (with-access::http-request req (path)
-	 (let ((base (hop-service-base)))
-	    (and (substring-at? base path 0)
-		 (let ((lp (string-length path))
-		       (lb (string-length base)))
-		    (or (=fx lp lb)
-			(and (=fx lp (+fx lb 1))
-			     (char=? (string-ref path (-fx lp 1))
-				     #\/))))))))
-   (define (maybe-autoload x)
-      (let ((url (make-file-name (hop-service-base) (cadr (assoc 'name x))))
-	    (path (cadr (assq 'weblet x)))
-	    (active (cadr (assq 'active x)))
-	    (autopred (assq 'autoload x)))
-	 (when active
-	    (if (pair? autopred)
-		(begin
-		   (hop-verb 2 "Setting autoload " path " on " (cadr autopred)
-			     "\n")
-		   (autoload path (eval (cadr autopred))))
-		(install-autoload-prefix path url)))))
-   (for-each (lambda (dir)
-		(for-each maybe-autoload (find-weblets dir)))
-	     dirs))
 
 ;; ----------------------------------------------------------------------
 ;; 	WEBLET-ABOUT ...
