02d6a9fe6af09ed68807497bbdfb6e8abe1a909e
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2006-09-13 21:01:06 +0200

    Start fixing a subtle bug on HSS loading.
    The HSS file are evaluated in the current module of the thread
    that loads them. This is obviously wrong. I have change this in order
    to load them in a fresh module that does not export anything. If a HSS
    wants to export something, it has to declare its own module. I think that
    this is the first step of a more general fix about HSS loading. I think that
    a problem remains due to parallel (multi-threading) load of HSS files.

--- runtime/make-lib.scm
+++ runtime/make-lib.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Wed Jan 18 10:49:38 2006                          */
-;*    Last change :  Fri Aug 25 08:59:20 2006 (serrano)                */
+;*    Last change :  Wed Sep 13 14:12:01 2006 (serrano)                */
 ;*    Copyright   :  2006 Manuel Serrano                               */
 ;*    -------------------------------------------------------------    */
 ;*    The module used to build the HOP heap file.                      */
