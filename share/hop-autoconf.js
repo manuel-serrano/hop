6ca73f8db39b31f0ac4e3ee76255a79a86583465
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2006-10-12 15:16:10 +0200

    Fix the PAN on IE7.

--- share/hop-autoconf.js
+++ share/hop-autoconf.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Thu May 18 05:26:40 2006                          */
-/*    Last change :  Thu Oct 12 14:29:26 2006 (serrano)                */
+/*    Last change :  Thu Oct 12 14:35:30 2006 (serrano)                */
 /*    Copyright   :  2006 Manuel Serrano                               */
 /*    -------------------------------------------------------------    */
 /*    All non portable components of the HOP runtime system. All other */
