ab909770bb9825d9a434371e59a4b51821669bb8
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2008-11-25 09:43:26 +0100

    Add US-ASCII to known charsets.

--- runtime/charset.scm
+++ runtime/charset.scm
@@ -1,9 +1,9 @@
 ;*=====================================================================*/
-;*    /users/serrano/prgm/project/hop/1.9.x/runtime/charset.scm        */
+;*    serrano/prgm/project/hop/1.10.x/runtime/charset.scm              */
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Wed Oct 10 06:46:43 2007                          */
-;*    Last change :  Thu Sep 11 15:06:11 2008 (root)                   */
+;*    Last change :  Tue Nov 25 08:40:27 2008 (serrano)                */
 ;*    Copyright   :  2007-08 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Functions for dealing with charset.                              */
@@ -36,6 +36,8 @@
 	       (iso-latin->utf8 str))
 	      ((UCS-2)
 	       (utf8-string->ucs2-string (iso-latin->utf8 str)))
+	      ((US-ASCII)
+	       (iso-8859-1->us-ascii str))
 	      (else
 	       str)))
 	  ((UTF-8)
@@ -45,6 +47,8 @@
 	       (utf8->iso-latin str))
 	      ((UCS-2)
 	       (utf8-string->ucs2-string str))
+	      ((US-ASCII)
+	       (iso-8859-1->us-ascii! (utf8->iso-latin str)))
 	      (else
 	       str)))
 	  ((UCS-2)
@@ -54,8 +58,14 @@
 	       (utf8->iso-latin! (ucs2-string->utf8-string str)))
 	      ((UTF-8)
 	       (ucs2-string->utf8-string str))
+	      ((US-ASCII)
+	       (iso-8859-1->us-ascii!
+		(utf8->iso-latin!
+		 (ucs2-string->utf8-string str))))
 	      (else
 	       str)))
+	  ((US-ASCII)
+	   (charset-convert (us-ascii->iso-latin str) 'ISO-LATIN-1 charset2))
 	  (else
 	   str))))
 
@@ -77,6 +87,8 @@
 	      ((UCS-2)
 	       (lambda (str)
 		  (utf8-string->ucs2-string (iso-latin->utf8 str))))
+	      ((US-ASCII)
+	       iso-8859-1->us-ascii)
 	      (else
 	       (lambda (x) x))))
 	  ((UTF-8)
@@ -87,6 +99,9 @@
 	      ((UCS-2)
 	       (lambda (str)
 		  (utf8-string->ucs2-string str)))
+	      ((US-ASCII)
+	       (lambda (s)
+		  (iso-8859-1->us-ascii! (utf8->iso-latin s))))
 	      (else
 	       (lambda (x) x))))
 	  ((UCS-2)
@@ -97,8 +112,24 @@
 		  (utf8->iso-latin! (ucs2-string->utf8-string str))))
 	      ((UTF-8)
 	       ucs2-string->utf8-string)
+	      ((US-ASCII)
+	       (lambda (str)
+		  (iso-8859-1->us-ascii!
+		   (utf8->iso-latin! (ucs2-string->utf8-string str)))))
 	      (else
 	       (lambda (x) x))))
+	  ((US-ASCII)
+	   (case charset2
+	      ((ISO-8859-1 ISO-8859-2 ISO-8859-15 ISO-LATIN-1
+			   WINDOWS-1250 WINDOWS-1252 WINDOWS-1256 WINDOWS-1258)
+	       (lambda (x) x))
+	      ((UTF-8)
+	       iso-latin->utf8)
+	      ((UCS-2)
+	       (lambda (str)
+		  (utf8-string->ucs2-string (iso-latin->utf8 str))))
+	      (else
+	       (lambda (x) x))))	  
 	  (else
 	   (lambda (x) x)))))
 
@@ -120,6 +151,8 @@
 	      ((UCS-2)
 	       (lambda (str)
 		  (utf8-string->ucs2-string (iso-latin->utf8 str))))
+	      ((US-ASCII)
+	       iso-8859-1->us-ascii!)
 	      (else
 	       (lambda (x) x))))
 	  ((UTF-8)
@@ -130,6 +163,9 @@
 	      ((UCS-2)
 	       (lambda (str)
 		  (utf8-string->ucs2-string str)))
+	      ((US-ASCII)
+	       (lambda (s)
+		  (iso-8859-1->us-ascii! (utf8->iso-latin! s))))
 	      (else
 	       (lambda (x) x))))
 	  ((UCS-2)
@@ -140,7 +176,152 @@
 		  (utf8->iso-latin! (ucs2-string->utf8-string str))))
 	      ((UTF-8)
 	       ucs2-string->utf8-string)
+	      ((US-ASCII)
+	       (lambda (str)
+		  (iso-8859-1->us-ascii!
+		   (utf8->iso-latin! (ucs2-string->utf8-string str)))))
 	      (else
 	       (lambda (x) x))))
+	  ((US-ASCII)
+	   (case charset2
+	      ((ISO-8859-1 ISO-8859-2 ISO-8859-15 ISO-LATIN-1
+			   WINDOWS-1250 WINDOWS-1252 WINDOWS-1256 WINDOWS-1258)
+	       iso-8859-1->us-ascii!)
+	      ((UTF-8)
+	       (lambda (str)
+		  (iso-latin->utf8! (iso-8859-1->us-ascii! str))))
+	      ((UCS-2)
+	       (lambda (str)
+		  (utf8-string->ucs2-string
+		   (iso-latin->utf8!
+		    (iso-8859-1->us-ascii! str)))))
+	      (else
+	       (lambda (x) x))))	  
 	  (else
 	   (lambda (x) x)))))
+
+;*---------------------------------------------------------------------*/
+;*    count-crlf ...                                                   */
+;*---------------------------------------------------------------------*/
+(define (count-crlf str)
+   (let ((len-1 (-fx (string-length str) 1)))
+      (let loop ((i 0)
+		 (n 0))
+	 (if (>=fx i len-1)
+	     n
+	     (let ((c (string-ref str i)))
+		(if (and (char=? c #\Return)
+			 (char=? (string-ref str (+fx i 1)) #\Newline))
+		    (loop (+fx i 2) (+fx n 1))
+		    (loop (+fx i 1) n)))))))
+
+;*---------------------------------------------------------------------*/
+;*    us-ascii->iso-latin-inner ...                                    */
+;*    -------------------------------------------------------------    */
+;*    Replace CRLF with NL                                             */
+;*---------------------------------------------------------------------*/
+(define (us-ascii->iso-latin-inner str res)
+   (let ((len (string-length str)))
+      (let loop ((i 0)
+		 (j 0))
+	 (if (=fx i len)
+	     res
+	     (let ((c (string-ref str i)))
+		(if (and (char=? c #\Return)
+			 (char=? (string-ref str (+fx i 1)) #\Newline))
+		    (begin
+		       (string-set! res j #\Newline)
+		       (loop (+fx i 2) (+fx j 1)))
+		    (begin
+		       (string-set! res j c)
+		       (loop (+fx i 1) (+fx j 1)))))))))
+
+;*---------------------------------------------------------------------*/
+;*    us-ascii->iso-latin ...                                          */
+;*    -------------------------------------------------------------    */
+;*    Just replace CRLF with NL                                        */
+;*---------------------------------------------------------------------*/
+(define (us-ascii->iso-latin str)
+   (let* ((len (string-length str))
+	  (count (count-crlf str))
+	  (res (make-string (-fx len count))))
+      (if (=fx count 0)
+	  (begin
+	     (blit-string! str 0 res 0 len)
+	     res)
+	  (us-ascii->iso-latin-inner str res))))
+
+;*---------------------------------------------------------------------*/
+;*    us-ascii->iso-latin! ...                                         */
+;*    -------------------------------------------------------------    */
+;*    Just replace CRLF with NL                                        */
+;*---------------------------------------------------------------------*/
+(define (us-ascii->iso-latin! str)
+   (let ((len (string-length str))
+	 (count (count-crlf str)))
+      (if (=fx count 0)
+	  str
+	  (let ((res (us-ascii->iso-latin-inner str str)))
+	     (string-shrink! res (-fx len count))))))
+
+;*---------------------------------------------------------------------*/
+;*    count-nl ...                                                     */
+;*---------------------------------------------------------------------*/
+(define (count-nl str)
+   (let ((len (string-length str)))
+      (let loop ((i 0)
+		 (n 0))
+	 (cond
+	    ((=fx i len)
+	     n)
+	    ((char=? (string-ref str i) #\Newline)
+	     (loop (+fx i 1) (+fx n 1)))
+	    (else
+	     (loop (+fx i 1) n))))))
+
+;*---------------------------------------------------------------------*/
+;*    iso-8859-1->us-ascii-inner ...                                   */
+;*    -------------------------------------------------------------    */
+;*    Replace NL with CRLF.                                            */
+;*---------------------------------------------------------------------*/
+(define (iso-8859-1->us-ascii-inner str res)
+   (let ((len (string-length str)))
+      (let loop ((i 0)
+		 (j 0))
+	 (if (=fx i len)
+	     res
+	     (let ((c (string-ref str i)))
+		(if (char=? c #\Return)
+		    (begin
+		       (string-set! res j #\Return)
+		       (string-set! res (+fx j 1) c)
+		       (loop (+fx i 1) (+fx j 2)))
+		    (begin
+		       (string-set! res j c)
+		       (loop (+fx i 1) (+fx j 1)))))))))
+
+;*---------------------------------------------------------------------*/
+;*    iso-8859-1->us-ascii ...                                         */
+;*    -------------------------------------------------------------    */
+;*    Just replace CRLF with NL                                        */
+;*---------------------------------------------------------------------*/
+(define (iso-8859-1->us-ascii str)
+   (let* ((len (string-length str))
+	  (count (count-nl str))
+	  (res (make-string (+fx len count))))
+      (if (=fx count 0)
+	  (begin
+	     (blit-string! str 0 res 0 len)
+	     res)
+	  (iso-8859-1->us-ascii-inner str res))))
+
+;*---------------------------------------------------------------------*/
+;*    iso-8859-1->us-ascii! ...                                        */
+;*---------------------------------------------------------------------*/
+(define (iso-8859-1->us-ascii! str)
+   (let* ((len (string-length str))
+	  (count (count-nl str)))
+      (if (=fx count 0)
+	  str
+	  (iso-8859-1->us-ascii-inner str (make-string (+fx len count))))))
+   
