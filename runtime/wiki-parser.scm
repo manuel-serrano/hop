7fdeab866b9ba47988a121cb7b0905f95547a851
Manuel Serrano (Manuel Serrano <Manuel Serrano (Manuel.Serrano@inria.fr)>
2009-05-07 08:49:02 +0200

    Fix Access Control documentation.

--- runtime/wiki-parser.scm
+++ runtime/wiki-parser.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Mon Apr  3 07:05:06 2006                          */
-;*    Last change :  Wed May  6 16:37:52 2009 (serrano)                */
+;*    Last change :  Thu May  7 08:44:17 2009 (serrano)                */
 ;*    Copyright   :  2006-09 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    The HOP wiki syntax tools                                        */
