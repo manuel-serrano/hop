7aa5b3ec726c679bbf840b80339fcffc77032004
Marcos Dione <mdione@sophia.inrira.fr>
2010-04-21 11:23:36 +0200

    * when compiling with --srfi boot-from-java, main is exported as main, and not as boot.

--- src/HopRunner.java
+++ src/HopRunner.java
@@ -8,13 +8,13 @@ import bigloo.*;
 public class HopRunner extends Object {
   public static int run (String []args) {
     // init all the modules before calling main
-    bigloo.hop.Main.bigloo_dlopen_init ();
+    bigloo.hop.main.bigloo_dlopen_init ();
     // and fake a proper initialization
     // bigloo.runtime.Llib.param.bigloo-initialized! ();
     // yes, 'tis mangled
     bigloo.runtime.Llib.param.BgL_bigloozd2initializa7edz12z67 ();
 
     Object list= foreign.listargv (args);
-    return bigloo.hop.Main.boot (list);
+    return bigloo.hop.main.main (list);
   }
 }
