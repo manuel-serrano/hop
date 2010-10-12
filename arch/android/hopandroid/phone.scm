e83510dab418575f48fd11f5cf01598b2d3b24c3
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-10-12 14:09:12 +0200

    .

--- arch/android/hopandroid/phone.scm
+++ arch/android/hopandroid/phone.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Tue Oct 12 12:30:23 2010                          */
-;*    Last change :  Tue Oct 12 12:30:45 2010 (serrano)                */
+;*    Last change :  Tue Oct 12 14:08:52 2010 (serrano)                */
 ;*    Copyright   :  2010 Manuel Serrano                               */
 ;*    -------------------------------------------------------------    */
 ;*    Android Phone implementation                                     */
@@ -16,4 +16,26 @@
 
    (library phone)
 
-   (export (class androidphone::phone)))
+   (export (class androidphone::phone
+	      (host::bstring read-only (default "localhost"))
+	      (port::int read-only (default 8081))
+	      (protocol::byte read-only (default 1))
+	      (%socket (default #unspecified)))))
+
+;*---------------------------------------------------------------------*/
+;*    phone-init ::androidphone ...                                    */
+;*---------------------------------------------------------------------*/
+(define-method (phone-init p::androidphone)
+   (with-access::androidphone p (port host %socket)
+      (set! %socket (make-client-socket host port))))
+
+;*---------------------------------------------------------------------*/
+;*    phone-vibrate ::androidphone ...                                 */
+;*---------------------------------------------------------------------*/
+(define-method (phone-vibrate p::androidphone duration::obj)
+   (with-access::androidphone p (protocol %socket)
+      (let ((op (socket-output-port %socket)))
+	 (write-byte protocol op)
+	 (write-char #\V op)
+	 (flush-output-port op))))
+   
