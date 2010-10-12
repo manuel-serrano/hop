26b2458003181e065855d0016bcaae73f3874a12
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-10-12 18:48:48 +0200

    .

--- arch/android/hopandroid/music.scm
+++ arch/android/hopandroid/music.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Tue Oct 12 12:31:01 2010                          */
-;*    Last change :  Tue Oct 12 12:31:18 2010 (serrano)                */
+;*    Last change :  Tue Oct 12 18:48:08 2010 (serrano)                */
 ;*    Copyright   :  2010 Manuel Serrano                               */
 ;*    -------------------------------------------------------------    */
 ;*    Android music implementation                                     */
@@ -16,4 +16,15 @@
 
    (library multimedia)
 
-   (export (class androidmusic::music)))
+   (export (class androidmusic::music
+	      (phone::androidphone read-only))))
+
+;*---------------------------------------------------------------------*/
+;*    music-play ::androidmusic ...                                    */
+;*---------------------------------------------------------------------*/
+(define-method (music-play m::androidmusic . song)
+   (with-access::androidphone p (protocol %socket)
+      (let ((op (socket-output %socket)))
+	 (write-byte protocol op)
+	 (write-char #\M op)
+	 (flush-output-port op))))
