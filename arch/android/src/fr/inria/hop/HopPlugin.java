88bace83c619171985425cab9bdcdb645eea5c3b
Manuel Serrano <Manuel.Serrano@inria.fr>
2010-10-22 11:30:06 +0200

    .

---
+++ arch/android/src/fr/inria/hop/HopPlugin.java
@@ -0,0 +1,37 @@
+/*=====================================================================*/
+/*    .../hop/2.2.x/arch/android/src/fr/inria/hop/HopPlugin.java       */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Tue Oct 19 09:38:21 2010                          */
+/*    Last change :  Tue Oct 19 19:06:59 2010 (serrano)                */
+/*    Copyright   :  2010 Manuel Serrano                               */
+/*    -------------------------------------------------------------    */
+/*    Root class for HopPlugins                                        */
+/*=====================================================================*/
+
+/*---------------------------------------------------------------------*/
+/*    The package                                                      */
+/*---------------------------------------------------------------------*/
+package fr.inria.hop;
+
+import android.app.*;
+import java.io.*;
+
+/*---------------------------------------------------------------------*/
+/*    The class                                                        */
+/*---------------------------------------------------------------------*/
+public abstract class HopPlugin {
+   // instance variables
+   HopAndroid handroid;
+   Activity activity;
+   public String name;
+
+   HopPlugin( HopAndroid h, Activity a, String n ) {
+      handroid = h;
+      activity = a;
+      name = n;
+   }
+
+   // the server
+   abstract void server( InputStream ip, OutputStream op ) throws IOException;
+}
