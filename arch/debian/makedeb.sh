21524c1905e80bea2d85226ddcb6dcf0d228405d
Manuel Serrano <Manuel.Serrano@inria.fr>
2011-12-17 09:50:19 +0100

    .

--- arch/debian/makedeb.sh
+++ arch/debian/makedeb.sh
@@ -4,7 +4,7 @@
 #*    -------------------------------------------------------------    */
 #*    Author      :  Manuel Serrano                                    */
 #*    Creation    :  Sat Dec 22 05:37:50 2007                          */
-#*    Last change :  Sat Dec 17 09:24:02 2011 (serrano)                */
+#*    Last change :  Sat Dec 17 09:50:29 2011 (serrano)                */
 #*    Copyright   :  2007-11 Manuel Serrano                            */
 #*    -------------------------------------------------------------    */
 #*    The Shell script to build the .deb for Hop on Maemo              */
@@ -16,7 +16,8 @@
 #*    Global configuration                                             */
 #*---------------------------------------------------------------------*/
 REPOSITORY=/users/serrano/prgm/distrib
-VERSION=2.3.0-pre1
+VERSION=2.3.0
+DISTRIB=-pre1
 BIGLOOVERSION=3.8a
 ICONS="hop-16x16.png hop-26x26.png hop-40x40.png hop-64x64.png"
 AUTHOR=Manuel.Serrano@inria.fr
@@ -40,7 +41,11 @@ mkdir -p $TMP
 #*---------------------------------------------------------------------*/
 #*    Untar the standard Hop version                                   */
 #*---------------------------------------------------------------------*/
-tar xfz $REPOSITORY/hop-$VERSION.tar.gz -C $TMP
+tar xfz $REPOSITORY/hop-$VERSION$DISTRIB.tar.gz -C $TMP
+
+if [ "$DISTRIB " != " " ]; then
+  mv $REPOSITORY/hop-$VERSION$DISTRIB $REPOSITORY/hop-$VERSION
+fi
 
 #*---------------------------------------------------------------------*/
 #*    The maemo configuration                                          */
