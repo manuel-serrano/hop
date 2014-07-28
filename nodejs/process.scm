4513af9bef73a62d132e468b4af328989216149d
Manuel Serrano <Manuel.Serrano@inria.fr>
2014-07-28 16:27:39 +0200

    .

--- nodejs/process.scm
+++ nodejs/process.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Thu Sep 19 15:02:45 2013                          */
-;*    Last change :  Mon Jul 28 16:05:53 2014 (serrano)                */
+;*    Last change :  Mon Jul 28 16:15:55 2014 (serrano)                */
 ;*    Copyright   :  2013-14 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    NodeJS process object                                            */
