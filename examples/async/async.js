ae78e4038e97b4bc4b193354ace9e3333e720274
Manuel Serrano <Manuel.Serrano@inria.fr>
2015-01-06 13:22:46 +0100

    .

--- examples/async/async.js
+++ examples/async/async.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Fri Apr 18 09:42:04 2014                          */
-/*    Last change :  Mon Jan  5 17:35:37 2015 (serrano)                */
+/*    Last change :  Tue Jan  6 13:21:52 2015 (serrano)                */
 /*    Copyright   :  2014-15 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    Simple example showing asynchronous response                     */
@@ -13,14 +13,23 @@
 /*=====================================================================*/
 var hop = require( "hop" );
 var fs = require( "fs" );
+var fontifier = require( hop.fontifier );
 
 service async() {
    return hop.HTTPResponseAsync(
       function( sendResponse ) {
          fs.readFile( async.resource( "async.js" ), "ascii",
                       function( err, data ) {
-                         sendResponse( <HTML> { <PRE> { data } } )
-                      } );
+                         sendResponse( <HTML> {
+			    <HEAD> {
+			       css: fontifier.css
+			    },
+			    <PRE> {
+			       class: "fontifier-prog",
+			       fontifier.hopFontifier( data )
+			    }
+			 } )
+		      } );
       },
    this );
 }
--- examples/tower/tower.js
+++ examples/tower/tower.js
@@ -3,15 +3,14 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Tue Jun  3 16:32:25 2014                          */
-/*    Last change :  Sun Dec 21 08:40:21 2014 (serrano)                */
-/*    Copyright   :  2014 Manuel Serrano                               */
+/*    Last change :  Tue Jan  6 13:22:27 2015 (serrano)                */
+/*    Copyright   :  2014-15 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    Recursive client-side tower                                      */
 /*    -------------------------------------------------------------    */
 /*    run: hop -v tower.js                                             */
 /*    browser: http://localhost:8080/hop/tower                         */
 /*=====================================================================*/
-
 var hop = require( 'hop' );
 
 service tower() {
