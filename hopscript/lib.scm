c339c06496b4bac9550d4d97ca34bd4844ccbbd2
Manuel Serrano <Manuel.Serrano@inria.fr>
2015-01-19 08:23:35 +0100

    .

--- hopscript/lib.scm
+++ hopscript/lib.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Tue Oct  8 08:16:17 2013                          */
-;*    Last change :  Sun Jan 18 09:50:57 2015 (serrano)                */
+;*    Last change :  Mon Jan 19 08:23:27 2015 (serrano)                */
 ;*    Copyright   :  2013-15 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    The Hop client-side compatibility kit (share/hop-lib.js)         */
@@ -16,8 +16,6 @@
 
    (library hop)
 
-   (include "arraybufferview.sch")
-   
    (import __hopscript_types
 	   __hopscript_property
 	   __hopscript_object
