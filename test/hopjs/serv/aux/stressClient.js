c8778d21a34e27e00f4c7d3f4473724e90b16081
Vincent Prunet <vincent.prunet@gmail.com>
2015-09-14 12:28:14 +0200

    Stress test for services (named args)
    fails in two cases:
    One client, lot of requests (about 20000 on OS X)
    Multiple clients, any number of requests.

---
+++ test/hopjs/serv/aux/stressClient.js
@@ -0,0 +1,50 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/3.0.x/test/hopjs/serv/stressClient.js   */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Vincent Prunet                                    */
+/*    Creation    :  Mon Sep  14 11:43:00 2015                         */
+/*    Last change :  Mon Sep  7 12:42:26 2015 (serrano)                */
+/*    Copyright   :  2015 Inria                                        */
+/*    -------------------------------------------------------------    */
+/*    Stress test for services: client worker                          */
+/*=====================================================================*/
+
+/* This worker iterates service invocations for <numCalls> times, then
+ * post a message to inform the main thread of completion */
+
+var hop = require( 'hop' );
+
+import service toTest();
+
+var numCalls = 10;
+var effectiveCalls = 0;
+
+onmessage = function( e ) {
+   var id = e.data;
+   console.log( 'client start', id );
+   test( id );
+};
+
+function test( id ) {
+   if (effectiveCalls == numCalls ) {
+      postMessage( id );
+   } else {
+      try {
+	 toTest( { id: id } ).post( function( result ) {
+	    effectiveCalls++;
+	    test( id );
+/*	 }, { fail: function( error ) {
+	    console.log( 'Service invocation failed for client %s at %s',
+			 id, effectiveCalls, error );
+	    //	 process.exit( 1 );
+	 }});
+*/
+	 });
+	 }
+      catch( e ) {
+	 console.log( 'client %s cannot post at %s', id, effectiveCalls );
+      }
+   };
+}
+
+
