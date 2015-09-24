764dea9a701aa57e0470b1944628f1f3fabda555
Vincent Prunet <vincent.prunet@gmail.com>
2015-09-24 11:45:29 +0200

    https tests, node_modules/config
    updated the TEST engine to read custom arguments for some tests (https).
    Fixed a typo in node_modules/config
    increased the worker test timeout .

--- test/hopjs/serv/https.js
+++ test/hopjs/serv/https.js
@@ -1,16 +1,7 @@
-/* TODO :
-   fix rc-file / server initialization order.
-   then replace this module by require( ./http.js ) */
-
 var hop = require( 'hop' );
 var config = require( hop.config );
 
-service foo( arg ) {
-   return arg ;
-}
-
-foo(1).post( function( result ) {
-   console.log( 'ok' );
-}, { ssl: true } );
-
 console.log( 'http: %s, https: %s', config.HTTPPort, config.HTTPSPort );
+
+require( './http.js' );
+
