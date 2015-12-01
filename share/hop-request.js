c7a18e4a20d63e874e27f9b3d3e2057cd59deddd
Manuel Serrano <Manuel.Serrano@inria.fr>
2015-12-01 06:09:30 +0100

    FormData support

--- share/hop-request.js
+++ share/hop-request.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Sat Dec 25 06:57:53 2004                          */
-/*    Last change :  Mon Nov 30 08:38:09 2015 (serrano)                */
+/*    Last change :  Mon Nov 30 17:57:00 2015 (serrano)                */
 /*    Copyright   :  2004-15 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    WITH-HOP implementation                                          */
@@ -92,10 +92,14 @@ function hop_apply_url( service, args ) {
          if( (args.length == 1) && hop_is_dom_form_element( args[ 0 ] ) ) {
 	    return hop_apply_form_url( service, args );
          } else {
-	    return service
-	       + "?hop-encoding=hop"
-   	       + "&vals=" + hop_bigloo_serialize( sc_vector2list( args ) );
-         }
+            if( (args.length == 1) && hop_is_dom_formdata_element( args[ 0 ] ) ) {
+	       return service;
+            } else {
+	       return service
+		  + "?hop-encoding=hop"
+   		  + "&vals=" + hop_bigloo_serialize( sc_vector2list( args ) );
+            }
+	 }
       }
    }
 }
@@ -509,7 +513,7 @@ function hop_request_onready( xhr, svc, succ, fail ) {
 /*    Hop ...                                                          */
 /*---------------------------------------------------------------------*/
 function Hop( svc, success, failure ) {
-   return withHOP( svc.url, success, failure, undefined, false );
+   return withHOP( svc.url, success, failure, undefined, false, null );
 }
 
 /*---------------------------------------------------------------------*/
@@ -539,17 +543,19 @@ HopFrame.prototype.toString = function() {
 
 HopFrame.prototype.post = function post( success, opt_or_fail ) {
    var svc = this.toString();
+   var arg = hop_is_dom_formdata_element( this.args[ 0 ] ) ?
+       this.args[ 0 ] : null;
 
    if( success ) {
       if( opt_or_fail instanceof Function || opt_or_fail == undefined ) {
-	 return withHOP( svc, success, opt_or_fail, this.options, false );
+	 return withHOP( svc, success, opt_or_fail, this.options, false, arg );
       } else {
-	 return withHOP( svc, success, false, opt_or_fail, false );
+	 return withHOP( svc, success, false, opt_or_fail, false, arg );
       }
    } else if( "Promise" in window ) {
       var frame = this;
       var promise = new Promise( function( resolve, reject ) {
-	 return withHOP( svc, resolve, reject, frame.options, false );
+	 return withHOP( svc, resolve, reject, frame.options, false, arg );
       } );
 
       if( hop_debug() > 0 ) {
@@ -587,7 +593,7 @@ HopFrame.prototype.hop_bigloo_serialize = hop_bigloo_serialize_hopframe;
 /*---------------------------------------------------------------------*/
 /*    withHOP ...                                                      */
 /*---------------------------------------------------------------------*/
-function withHOP( svc, success, fail, opt, force_sync ) {
+function withHOP( svc, success, fail, opt, force_sync, arg ) {
    var sync = force_sync;
    var anim = true;
    var header = false;
@@ -602,7 +608,9 @@ function withHOP( svc, success, fail, opt, force_sync ) {
 
    return hop_send_request( svc, sync, success, fail, anim,
 			    hop_serialize_request_env(),
-			    false, false, false, false, header, "javascript" );
+			    false, false, false, false, header,
+			    "javascript",
+			    arg );
 }
 			    
 /*---------------------------------------------------------------------*/
@@ -612,7 +620,7 @@ function withHOP( svc, success, fail, opt, force_sync ) {
 /*    functions.                                                       */
 /*---------------------------------------------------------------------*/
 /*** META ((export #t) (arity #t)) */
-function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, loc, header, idiom ) {
+function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, loc, header, idiom, arg ) {
    var xhr = x ? x : hop_make_xml_http_request();
 
    /* MS, 20 Jun 08: I cannot understand why but sometime global functions */
@@ -630,7 +638,7 @@ function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, 
    }
 
    if( !sync ) {
-      xhr.open( "PUT", svc, true );
+      xhr.open( arg ? "POST" : "PUT", svc, true );
 
       if( ("responseType" in xhr) && hop_config.uint8array ) {
 	 xhr.onload = onreadystatechange;
@@ -645,7 +653,7 @@ function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, 
 	 xhr.setRequestHeader( "Hop-Serialize", "javascript" );
       }
    } else {
-      xhr.open( "PUT", svc, false );
+      xhr.open( arg ? "POST" : "PUT", svc, false );
       
       xhr.onreadystatechange = onreadystatechange;
       xhr.setRequestHeader( "Hop-Serialize", "javascript" );
@@ -694,35 +702,38 @@ function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, 
          xhr.setRequestHeader( 'Hop-Debug-Stack', hop_bigloo_serialize( stk ) );
       }
    }
-   
-   xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' );
-   if( hop_config.navigator_family != "safari" &&
-       hop_config.navigator_family != "chrome" &&
-       hop_config.navigator_family != "webkit" ) {
-      xhr.setRequestHeader( 'Connection', 'close' );
-   }
-   if( henv.length > 0 ) {
-      xhr.setRequestHeader( 'Hop-Env', henv );
-   }
-   if( header instanceof Object ) {
-      for( var k in header ) {
-	 xhr.setRequestHeader( k, header[ k ] );
+
+   if( arg == null ) {
+      xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' );
+      if( hop_config.navigator_family != "safari" &&
+	  hop_config.navigator_family != "chrome" &&
+	  hop_config.navigator_family != "webkit" ) {
+	 xhr.setRequestHeader( 'Connection', 'close' );
+      }
+      if( henv.length > 0 ) {
+	 xhr.setRequestHeader( 'Hop-Env', henv );
+      }
+      if( header instanceof Object ) {
+	 for( var k in header ) {
+	    xhr.setRequestHeader( k, header[ k ] );
+	 }
+      }
+      if( ("multipart" in xhr) && (xhr.multipart === true) ) {
+	 /* This header is needed to let the server */
+	 /* disable timeout for this connection     */
+	 xhr.setRequestHeader( 'Xhr-Multipart', "true" );
       }
    }
+   
    if( auth ) {
       xhr.setRequestHeader( 'Authorization', auth );
-   }
-   if( ("multipart" in xhr) && (xhr.multipart === true) ) {
-      /* This header is needed to let the server */
-      /* disable timeout for this connection     */
-      xhr.setRequestHeader( 'Xhr-Multipart', "true" );
    }
 
    xhr.svc = svc;
    xhr.onerror = function( e ) { return fail( e, xhr ) };
    
    try {
-      xhr.send( null );
+      xhr.send( arg );
 
       if( anim ) {
 	 var a = (anim instanceof Function) ? anim : hop_default_anim_get();
@@ -760,62 +771,6 @@ function hop_send_request( svc, sync, success, failure, anim, henv, auth, t, x, 
 
       return fail( e, xhr );
    }
-
-   return xhr;
-}
-
-/*---------------------------------------------------------------------*/
-/*    hop_send_request_xdomain ...                                     */
-/*    -------------------------------------------------------------    */
-/*    In this function SUCCESS and FAILURE are *always* bound to       */
-/*    functions.                                                       */
-/*---------------------------------------------------------------------*/
-function hop_send_request_xdomain( e ) {
-   var xhr = hop_make_xml_http_request();
-   var m = e.data.match( "([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*)" );
-   var key = m[ 1 ];
-   var svc = m[ 2 ];
-   var henv = m[ 3 ];
-   var auth = m[ 4 ];
-   var header = (typeof m[ 5 ] == "string" ? JSON.parse( m[ 5 ] ) : false);
-
-   function onreadystatechange() {
-      if( xhr.readyState == 4 ) {
-	 var ctype = hop_header_content_type( xhr );
-	 var serialize = hop_header_hop_serialize( xhr );
-
-	 e.source.postMessage(
-	    key
-	       + " " + xhr.status
-	       + " " + ctype
-	       + " " + serialize 
-	       + " " + xhr.responseText,
-	    e.origin );
-      }
-   }
-
-   xhr.onreadystatechange = onreadystatechange;
-   xhr.open( "PUT", svc, true );
-   
-   xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=ISO-8859-1' );
-   if( hop_config.navigator_family != "safari" &&
-       hop_config.navigator_family != "chrome" &&
-       hop_config.navigator_family != "webkit" ) {
-      xhr.setRequestHeader( 'Connection', 'close' );
-   }
-   if( henv.length > 0 ) {
-      xhr.setRequestHeader( 'Hop-Env', henv );
-   }
-   if( header instanceof Object ) {
-      for( var k in header ) {
-	 xhr.setRequestHeader( k, header[ k ] );
-      }
-   }
-   if( auth.length > 0 ) {
-      xhr.setRequestHeader( 'Authorization', auth );
-   }
-
-   xhr.send( null );
 
    return xhr;
 }
@@ -905,7 +860,7 @@ function with_hop( svc, success, failure, sync, anim, timeout, header ) {
    return hop_send_request( svc, sync,
 			    success, failure,
 			    anim, hop_serialize_request_env(), false,
-			    timeout, header, "hop" );
+			    timeout, header, "hop", null );
 }
 
 /*---------------------------------------------------------------------*/
@@ -945,7 +900,8 @@ function with_hop( svc, success, failure, sync, anim, timeout, header ) {
 			(string-append "Basic "
 			   (base64-encode (string-append user ":" password)))))
 		   ,timeout
-		   ,header)
+		   ,header
+		   '())
 		 ;; same origin with-hop
 		 `((@ hop-send-request __hop)
 		   ,svc
@@ -964,7 +920,8 @@ function with_hop( svc, success, failure, sync, anim, timeout, header ) {
 		   #f
 		   ,(when (epair? svc) `',(cer svc))
 		   ,header
-		   "hop")))
+		   "hop"
+		   '())))
 	    ((eq? (car rest) :anim)
 	     (if (null? (cdr rest))
 		 (error 'with-hop "Illegal :anim argument" rest)
