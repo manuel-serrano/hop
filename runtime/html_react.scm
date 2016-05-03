26f15b4efd972b054bbf7a4ddc725e8480987602
Manuel Serrano <Manuel.Serrano@inria.fr>
2016-05-03 21:05:22 +0200

    Fix strings

--- runtime/html_react.scm
+++ runtime/html_react.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Thu Apr 28 18:01:20 2016                          */
-;*    Last change :  Fri Apr 29 17:15:01 2016 (serrano)                */
+;*    Last change :  Thu May  5 06:12:28 2016 (serrano)                */
 ;*    Copyright   :  2016 Manuel Serrano                               */
 ;*    -------------------------------------------------------------    */
 ;*    Dynamic nodes                                                    */
@@ -28,6 +28,7 @@
 	    __hop_charset
 	    __hop_xml-types
 	    __hop_xml
+	    __hop_dom
 	    __hop_hop
 	    __hop_cache
 	    __hop_user
@@ -53,7 +54,26 @@
 				       (xml-tilde->sexp node)))
 			body))))))
 	       
-	 
+;*---------------------------------------------------------------------*/
+;*    xml-write ::xml-react ...                                        */
+;*---------------------------------------------------------------------*/
+(define-method (xml-write obj::xml-react p backend)
    
+   (define (react-init key parent thunk)
+      (if (not parent)
+	  (error "react" "illegal orphan react node" obj)
+	  (with-access::xml-element parent ((pid id))
+	     (cond
+		((dom-next-sibling obj)
+		 =>
+		 (lambda (sibling)
+		    (with-access::xml-element sibling ((sid id))
+		       `((pragma "window.hop.reactInit") ,pid ,sid ,key ,thunk))))
+		(else
+		 `((pragma "window.hop.reactInit") ,pid ,#f ,key ,thunk))))))
 
-
+   (with-access::xml-react obj (id body parent)
+      (let ((expr (react-init id parent body)))
+	 (xml-write (sexp->xml-tilde expr) p backend))))
+   
+   
