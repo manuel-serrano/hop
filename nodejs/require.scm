a8cb8d05c1aa46c4c454209e20a12f6c8c5a2ae3
Manuel Serrano <Manuel.Serrano@inria.fr>
2017-03-29 17:57:47 +0200

    Background compilation fix

--- nodejs/require.scm
+++ nodejs/require.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Mon Sep 16 15:47:40 2013                          */
-;*    Last change :  Wed Mar 29 08:30:20 2017 (serrano)                */
+;*    Last change :  Wed Mar 29 17:28:09 2017 (serrano)                */
 ;*    Copyright   :  2013-17 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Native Bigloo Nodejs module implementation                       */
@@ -799,7 +799,10 @@
 	     (proc (synchronize socompile-mutex
 		      (unless socompile-ended
 			 (register-socompile-process!
-			    (apply run-process (car line) :wait #f error: pipe:
+			    (apply run-process (car line)
+			       :wait #f
+			       error: pipe:
+			       output: "/dev/null"
 			       (cdr line))
 			    cmd ksucc kfail))))
 	     
@@ -872,9 +875,9 @@
 	       ((eq? tmp 'loop) (loop))
 	       (else
 		(let* ((sopath (hop-sofile-path filename))
-		       (sopathtmp (hop-sofile-path
-				     (string-append "#"
-					(prefix (basename filename)))))
+		       (sopathtmp (make-file-name
+				     (dirname sopath)
+				     (string-append "#" (basename sopath))))
 		       (cmd (format "~a ~a -y -v3 --js-no-module-main -o ~a ~a"
 			       (hop-hopc)
 			       filename sopathtmp
@@ -883,6 +886,7 @@
 		       (kfail (make-kfail sopath sopathtmp)))
 		   (make-directories (dirname sopath))
 		   (trace-item "sopath=" sopath)
+		   (trace-item "sopathtmp=" sopathtmp)
 		   (trace-item "cmd=" cmd)
 		   (hop-verb 3 (hop-color -1 -1 " COMPILE") " " cmd "\n")
 		   (let ((msg (exec cmd ksucc kfail)))
