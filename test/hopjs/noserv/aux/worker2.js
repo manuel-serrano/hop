a156f4f19ab230e5bc363c0770cff874c08a2765
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-01-26 06:22:15 +0100

    .

--- test/hopjs/noserv/aux/worker2.js
+++ test/hopjs/noserv/aux/worker2.js
@@ -1,16 +1,20 @@
 /*=====================================================================*/
-/*    .../project/hop/3.0.x/test/hopjs/noserv/aux/worker2.js           */
+/*    .../prgm/project/hop/3.1.x/test/hopjs/noserv/aux/worker2.js      */
 /*    -------------------------------------------------------------    */
 /*    Author      :  Vincent Prunet                                    */
 /*    Creation    :  Mon Sep  28 18:43:00 2015                         */
-/*    Last change :  Tue Sep  8 18:10:37 2015 (serrano)                */
-/*    Copyright   :  2015 Inria                                        */
+/*    Last change :  Fri Jan 26 06:21:56 2018 (serrano)                */
+/*    Copyright   :  2015-18 Inria                                     */
 /*    -------------------------------------------------------------    */
 /*    Testing workers                                                  */
 /*=====================================================================*/
 
 onmessage = function( e ) {
-   console.log( 'worker processing ', e.data );
-   postMessage( 'Response' );
+   if( e.data.length == 6 ) {
+      e.data.push( 7 );
+      postMessage( e.data );
+   } else {
+      throw "bad slave value " + e.data;
+   }
    close();
 };
