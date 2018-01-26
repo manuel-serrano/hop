3af54facbab45d4bc3df439aa650a40377ef0fdc
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-01-26 08:32:32 +0100

    Extend requirec example

--- examples/requirec/requirec.js
+++ examples/requirec/requirec.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Fri Apr 18 09:42:04 2014                          */
-/*    Last change :  Fri Jan 26 06:36:42 2018 (serrano)                */
+/*    Last change :  Fri Jan 26 08:00:28 2018 (serrano)                */
 /*    Copyright   :  2014-18 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    multitier require                                                */
@@ -13,10 +13,11 @@
 /*=====================================================================*/
 service requirec() {
    return <html> 
-     <head module= ${[ "./mod1.js", "./mod2.js", "./example.json" ]}>
+     <head module= ${[ "./mod1.js", "./mod2.js", "./example.json", "./mod.html" ]}>
        <script defer>
 	  var mod1 = require( "./mod1.js" );
 	  var ex = require( "./example.json" );
+	  var dhtml = require( "./mod.html" );
        </script>
      </head>
      <button onclick=~{ document.body.appendChild( mod1.hello() ) }>
@@ -24,6 +25,9 @@ service requirec() {
      </button>
      <button onclick=~{ alert( "desc=" + ex.description ) }>
        json me
+     </button>
+     <button onclick=~{ alert( "desc=" + dhtml.outerHTML ) }>
+       html me
      </button>
    </html>;
 }
