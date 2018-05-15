3c8be186cb537b1bb6536500ea88856c7d687bf8
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-05-15 11:31:59 +0200

    Fix async keyword

--- js2scheme/lexer.scm
+++ js2scheme/lexer.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Sun Sep  8 07:33:09 2013                          */
-;*    Last change :  Tue May 15 11:22:25 2018 (serrano)                */
+;*    Last change :  Tue May 15 11:31:40 2018 (serrano)                */
 ;*    Copyright   :  2013-18 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    JavaScript lexer                                                 */
@@ -44,8 +44,7 @@
 (define *JS-care-future-reserved* #t)
 
 (define *keyword-list*
-   '(;;"async"
-     "await"
+   '("await"
      "break"
      "case"
      "catch"
