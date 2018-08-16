81fb6a272924473c8865579666a047235fc2bb6d
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-08-16 15:58:59 +0200

    .

--- githooks/autobench/measure.sh
+++ githooks/autobench/measure.sh
@@ -3,6 +3,8 @@
 system=$1
 benchmark=$2
 
+[ -z "$PERF" ] && PERF=perf
+
 mkdir -p $GITHOOKS_DIR/autobench/results/$system/$HOSTNAME
 
 times=
@@ -13,8 +15,9 @@ sep="["
 #* export TIMEFORMAT                                                   */
 
 for ((i=0; i<$AUTOBENCH_ITER; i++)) do
-  tm=`/usr/bin/time -f '%e' $TMP/$benchmark 2>&1 > /dev/null`
-  cy=`$GITHOOKS_DIR/autobench/perfcycles.sh $TMP/$benchmark 2>&1`
+  p=`$PERF stat $TMP/$benchmark 2> $TMP/perf.run > /dev/null`
+  cy=`grep "   cycles  " $TMP/perf.run | awk '{print $1}' | sed 's/,//g'`
+  tm=`grep " seconds time elapsed" $TMP/perf.run | awk '{print $1}'`
 
   times="$times$sep $tm"
   cycles="$cycles$sep $cy"
@@ -22,6 +25,7 @@ for ((i=0; i<$AUTOBENCH_ITER; i++)) do
   sep=","
 done
 
+echo "$2:"
 echo "{ times: $times ], cycles: $cycles ] }" \
   | tee $GITHOOKS_DIR/autobench/results/$system/$HOSTNAME/$benchmark.json
 
