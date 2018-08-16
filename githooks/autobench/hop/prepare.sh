2f3cda1c491ad2cfadc0772454272806b48d98a0
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-08-16 15:19:59 +0200

    .

--- githooks/autobench/hop/prepare.sh
+++ githooks/autobench/hop/prepare.sh
@@ -4,11 +4,11 @@
 #*    -------------------------------------------------------------    */
 #*    Author      :  Manuel Serrano                                    */
 #*    Creation    :  Mon Aug 13 16:19:56 2018                          */
-#*    Last change :  Thu Aug 16 14:49:44 2018 (serrano)                */
+#*    Last change :  Thu Aug 16 15:19:41 2018 (serrano)                */
 #*    Copyright   :  2018 Manuel Serrano                               */
 #*    -------------------------------------------------------------    */
 #*    Benchmarks compilation                                           */
 #*=====================================================================*/
 
-$PROJECT_DIR/autobench/local/bin/hopc -Ox -o $TMP/$2 $PROJECT_DIR/bench/$2.js
+$GITHOOKS_DIR/autobench/local/bin/hopc -Ox -o $TMP/$2 $PROJECT_DIR/bench/$2.js
 
