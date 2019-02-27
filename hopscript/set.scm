38cad984e5795edbaff65798bb3566364e85d8e1
Manuel Serrano <Manuel.Serrano@inria.fr>
2019-02-27 08:27:08 +0100

    .

---
+++ hopscript/set.scm
@@ -0,0 +1,454 @@
+;*=====================================================================*/
+;*    serrano/prgm/project/hop/3.2.x/hopscript/set.scm                 */
+;*    -------------------------------------------------------------    */
+;*    Author      :  Manuel Serrano                                    */
+;*    Creation    :  Mon Feb 25 13:32:40 2019                          */
+;*    Last change :  Wed Feb 27 08:10:37 2019 (serrano)                */
+;*    Copyright   :  2019 Manuel Serrano                               */
+;*    -------------------------------------------------------------    */
+;*    Native Bigloo support of JavaScript MAP object.                  */
+;*    -------------------------------------------------------------    */
+;*    https://www.ecma-international.org/ecma-262/6.0/#sec-map-objects */
+;*    https://developer.mozilla.org/en-US/docs/Web/JavaScript/         */
+;*       Reference/Global_Objects/Map                                  */
+;*=====================================================================*/
+
+;*---------------------------------------------------------------------*/
+;*    The module                                                       */
+;*---------------------------------------------------------------------*/
+(module __hopscript_set
+   
+   (include "../nodejs/nodejs_debug.sch")
+   
+   (library hop)
+   
+   (include "types.sch" "stringliteral.sch")
+   
+   (import __hopscript_types
+	   __hopscript_arithmetic
+	   __hopscript_lib
+	   __hopscript_object
+	   __hopscript_function
+	   __hopscript_property
+	   __hopscript_private
+	   __hopscript_public
+	   __hopscript_regexp
+	   __hopscript_array
+	   __hopscript_error
+	   __hopscript_generator
+	   __hopscript_worker
+	   __hopscript_spawn)
+   
+   (export (js-init-set! ::JsGlobalObject)
+	   (js-init-weakset! ::JsGlobalObject)))
+
+;*---------------------------------------------------------------------*/
+;*    global parameters                                                */
+;*---------------------------------------------------------------------*/
+(define-inline (DEFAULT-EMPTY-VECTOR-SIZE) 4)
+
+;*---------------------------------------------------------------------*/
+;*    js-init-set! ...                                                 */
+;*---------------------------------------------------------------------*/
+(define (js-init-set! %this::JsGlobalObject)
+   (init-set! %this 'Set init-builtin-set-prototype! 'none))
+
+;*---------------------------------------------------------------------*/
+;*    js-init-weakset! ...                                             */
+;*---------------------------------------------------------------------*/
+(define (js-init-weakset! %this::JsGlobalObject)
+   (init-set! %this 'WeakSet init-builtin-weakset-prototype! 'keys))
+
+;*---------------------------------------------------------------------*/
+;*    init-set! ...                                                    */
+;*---------------------------------------------------------------------*/
+(define (init-set! %this::JsGlobalObject name init-prototype! weak::symbol)
+   
+   (with-access::JsGlobalObject %this (__proto__ js-function-prototype)
+      
+      (define (%js-set this . args)
+	 (js-raise-type-error %this
+	    (format "Constructor ~a requires 'new'" name) this))
+      
+      (define (js-set-alloc %this constructor::JsFunction)
+	 (instantiateJsMap
+	    (__proto__ (js-get constructor 'prototype %this))
+	    (mapdata (create-hashtable
+			:weak weak
+			:eqtest (lambda (x y) (js-same-value-zero? x y %this))))
+	    (vec (make-vector (DEFAULT-EMPTY-VECTOR-SIZE) (js-absent)))
+	    (cursor -1)))
+      
+      (define js-set-prototype
+	 (instantiateJsObject
+	    (__proto__ __proto__)))
+      
+      (define js-set
+	 (js-make-function %this %js-set 0 (symbol->string! name)
+	    :__proto__ js-function-prototype
+	    :prototype js-set-prototype
+	    :size 0
+	    :alloc js-set-alloc
+	    :construct (lambda (this . iterable)
+			  (if (pair? iterable)
+			      (js-set-construct %this this (car iterable))
+			      this))))
+      
+      ;; init the prototype properties
+      (init-prototype! %this js-set js-set-prototype)
+      
+      ;; bind SET/WEAKSET in the global object
+      (js-bind! %this %this name
+	 :configurable #f :enumerable #f :value js-set
+	 :hidden-class #t)
+      
+      ;; @@species
+      ;; https://www.ecma-international.org/ecma-262/6.0/#sec-get-set-@@species
+      (with-access::JsGlobalObject %this (js-symbol-species)
+	 (js-bind! %this js-set js-symbol-species
+	    :get (js-make-function %this (lambda (this) js-set)
+		    0 "get [Symbol.species]")
+	    :enumerable #f
+	    :configurable #t))
+      
+      js-set))
+
+;*---------------------------------------------------------------------*/
+;*    js-set-construct ...                                             */
+;*---------------------------------------------------------------------*/
+(define (js-set-construct %this this::JsMap iterable)
+
+   (define (js-set-construct-iterator this::JsMap iter next)
+      (let ((add (js-get this 'add %this)))
+	 (let loop ()
+	    (let ((ni (js-call0 %this next iter)))
+	       (cond
+		  ((not ni)
+		   this)
+		  ((js-totest (js-get ni "done" %this))
+		   this)
+		  (else
+		   (let ((value (js-get ni "value" %this)))
+		      (js-call1 %this add this value)
+		      (loop))))))))
+   
+   (define (js-set-construct-iterable this::JsMap iterable)
+
+      (define (close-iterator iter)
+	 (let ((return (js-get iter 'return %this)))
+	    (when (isa? return JsFunction)
+	       (js-call0 %this return iter))))
+      
+      (with-access::JsGlobalObject %this (js-symbol-iterator)
+	 (let ((i (js-get iterable js-symbol-iterator %this)))
+	    (if (isa? i JsFunction)
+		(let ((iter (js-call0 %this i iterable)))
+		   (if (js-object? iter)
+		       (let ((next (js-get iter 'next %this)))
+			  (if (not (isa? next JsFunction))
+			      (js-raise-type-error %this
+				 "Illegal IteratorValue ~a" next)
+			      (with-handler
+				 (lambda (e)
+				    (close-iterator iter)
+				    (raise e))
+				 (js-set-construct-iterator
+				    this iter next))))
+		       (js-raise-type-error
+			  %this "Wrong iterator" iter)))
+		(js-raise-type-error %this "Not an iterator" i)))))
+   
+   (define (js-set-construct-array this::JsMap iterable::JsArray)
+      (let ((len (js-array-length iterable))
+	    (add (js-get this 'add %this)))
+	 (let loop ((i #u32:0))
+	    (if (=u32 i len)
+		this
+		(let ((value (js-array-index-ref iterable i %this)))
+		   (js-call1 %this add this value)
+		   (loop (+u32 i #u32:1)))))))
+   
+   (cond
+      ((or (eq? iterable (js-undefined)) (eq? iterable '()))
+       this)
+      ((isa? iterable JsArray)
+       (js-set-construct-array this iterable))
+      (else
+       (js-set-construct-iterable this iterable))))
+
+;*---------------------------------------------------------------------*/
+;*    init-builtin-set-prototype! ...                                  */
+;*---------------------------------------------------------------------*/
+(define (init-builtin-set-prototype! %this js-set js-set-prototype)
+
+   ;; add
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.set
+   (define (js-set-add this key)
+
+      (define (get-index! this::JsMap)
+	 (with-access::JsMap this (mapdata vec cursor)
+	    (let ((idx (+fx cursor 1)))
+	       (set! cursor idx)
+	       (if (<fx idx (vector-length vec))
+		   idx
+		   (let ((nvec (copy-vector vec (*fx (vector-length vec) 2))))
+		      (vector-fill! nvec (js-absent) (vector-length vec))
+		      (set! vec nvec)
+		      idx)))))
+	 
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (mapdata vec)
+	     (let ((k (if (and (flonum? key)
+			       (=fl key 0.0)
+			       (=fx (signbitfl key) 1))
+			  0.0
+			  key)))
+		(let ((idx (hashtable-get mapdata k)))
+		   (unless (fixnum? idx)
+		      (let ((idx (get-index! this)))
+			 (hashtable-put! mapdata k idx)
+			 (vector-set! vec idx k))))
+		this))
+	  (js-raise-type-error %this "not a Set" this)))
+   
+   (js-bind! %this js-set-prototype 'add
+      :value (js-make-function %this js-set-add 1 "add"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; clear
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.clear
+   (define (map-prototype-clear this key)
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (mapdata vec cursor)
+	     (hashtable-clear! mapdata)
+	     (set! vec (make-vector (DEFAULT-EMPTY-VECTOR-SIZE) (js-absent)))
+	     (set! cursor 0))
+	  (js-raise-type-error %this "Not a Set" this)))
+   
+   (js-bind! %this js-set-prototype 'clear
+      :value (js-make-function %this map-prototype-clear 0 "clear"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; constructor
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.constructor
+   (js-bind! %this js-set-prototype 'constructor
+      :value js-set :enumerable #f
+      :hidden-class #t)
+
+   ;; delete
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.delete
+   (js-bind! %this js-set-prototype 'delete
+      :value (js-make-function %this (js-set-delete %this) 1 "delete"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+
+   ;; entries
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.entries
+   (define (js-set-entries this)
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (vec)
+	     (js-make-vector-iterator vec
+		(lambda (%this val)
+		   (js-vector->jsarray (vector val val) %this))
+		%this))
+	  (js-raise-type-error %this "Not a Sap" this)))
+      
+   (js-bind! %this js-set-prototype 'entries
+      :value (js-make-function %this js-set-entries 0 "entries"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; forEach
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.foreach
+   (define (js-set-for-each this fn . thisarg)
+      (if (isa? this JsMap)
+	  (if (isa? fn JsFunction)
+	      (let ((t (if (pair? thisarg) (car thisarg) (js-undefined))))
+		 (with-access::JsMap this (vec)
+		    (let loop ((i 0))
+		       (when (<fx i (vector-length vec))
+			  (let ((v (vector-ref vec i)))
+			     (unless (js-absent? v)
+				(js-call3 %this fn t v v this)))
+			  (loop (+fx i 1))))))
+	      (js-raise-type-error %this "Not a function" fn))
+	  (js-raise-type-error %this "not a Set" this)))
+
+   (js-bind! %this js-set-prototype 'forEach
+      :value (js-make-function %this js-set-for-each 1 "forEach"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; has
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.has
+   (js-bind! %this js-set-prototype 'has
+      :value (js-make-function %this (js-set-has %this) 1 "has"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+
+   ;; keys
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.keys
+   (js-bind! %this js-set-prototype 'keys
+      :value (js-make-function %this js-set-values 0 "keys"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; size
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-get-set.prototype.size
+   (define (js-set-size this)
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (mapdata)
+	     (hashtable-size mapdata))
+	  (js-raise-type-error %this "Not a Set" this)))
+   
+   (js-bind! %this js-set-prototype 'size
+      :get (js-make-function %this js-set-size 0 "size"
+	      :prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; values
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-map.prototype.values
+   (define (js-set-values this)
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (vec)
+	     (js-make-vector-iterator vec (lambda (%this val) val) %this))
+	  (js-raise-type-error %this "Not a Set" this)))
+   (js-bind! %this js-set-prototype 'values
+      :value (js-make-function %this js-set-values 0 "values"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; @@iterator
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype-@@tostringtag
+   (with-access::JsGlobalObject %this (js-symbol-iterator)
+      (js-bind! %this js-set-prototype js-symbol-iterator
+	 :value (js-make-function %this js-set-values 0 "values"
+		   :prototype (js-undefined))
+	 :enumerable #f
+	 :configurable #t))
+      
+   ;; @@toStringTag
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype-@@tostringtag
+   (with-access::JsGlobalObject %this (js-symbol-tostringtag)
+      (js-bind! %this js-set-prototype js-symbol-tostringtag
+	 :value (js-ascii->jsstring "Set")
+	 :enumerable #f
+	 :configurable #t))
+      
+   )
+
+;*---------------------------------------------------------------------*/
+;*    init-builtin-weakset-prototype! ...                              */
+;*---------------------------------------------------------------------*/
+(define (init-builtin-weakset-prototype! %this js-set js-set-prototype)
+
+   ;; add
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.add
+   (define (js-set-add this key)
+      
+      (define (get-index! this::JsMap)
+	 (with-access::JsMap this (mapdata vec cursor)
+	    (let ((idx (+fx cursor 1)))
+	       (set! cursor idx)
+	       (if (<fx idx (vector-length vec))
+		   idx
+		   (let ((nvec (copy-vector vec (*fx (vector-length vec) 2))))
+		      (vector-fill! nvec (vector-length vec) (js-absent))
+		      (set! vec nvec)
+		      idx)))))
+      
+      (cond
+	 ((not (isa? key JsObject))
+	  (js-raise-type-error %this "not an object" key))
+	 ((isa? this JsMap)
+	  (with-access::JsMap this (mapdata vec)
+	     (let ((k (if (and (flonum? key)
+			       (=fl key 0.0)
+			       (=fx (signbitfl key) 1))
+			  0.0
+			  key)))
+		(let ((idx (hashtable-get mapdata k)))
+		   (if (fixnum? idx)
+		       (weakptr-data-set! (vector-ref vec idx) k)
+		       (let ((idx (get-index! this)))
+			  (hashtable-put! mapdata k idx)
+			  (vector-set! vec idx (make-weakptr k)))))
+		this)))
+	 (else
+	  (js-raise-type-error %this "not a Set" this))))
+   
+   (js-bind! %this js-set-prototype 'add
+      :value (js-make-function %this js-set-add 2 "add"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+   
+   ;; constructor
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.constructor
+   (js-bind! %this js-set-prototype 'constructor
+      :value js-set :enumerable #f
+      :hidden-class #t)
+
+   ;; delete
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.delete
+   (js-bind! %this js-set-prototype 'delete
+      :value (js-make-function %this (js-set-delete %this) 1 "delete"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+
+   ;; has
+   ;; https://www.ecma-international.org/ecma-262/6.0/#sec-set.prototype.has
+   (js-bind! %this js-set-prototype 'has
+      :value (js-make-function %this (js-set-has %this) 1 "has"
+		:prototype (js-undefined))
+      :enumerable #f
+      :hidden-class #t)
+
+   )
+
+;*---------------------------------------------------------------------*/
+;*    js-set-delete ...                                                */
+;*    -------------------------------------------------------------    */
+;*    https://www.ecma-international.org/ecma-262/6.0/                 */
+;*       #sec-set.prototype.delete                                     */
+;*---------------------------------------------------------------------*/
+(define (js-set-delete %this)
+   (lambda (this key)
+      (if (isa? this JsMap)
+	  (with-access::JsMap this (mapdata vec)
+	     (let ((idx (hashtable-get mapdata key)))
+		(when idx
+		   (hashtable-remove! mapdata key)
+		   (vector-set! vec idx (js-absent))
+		   #t)))
+	  (js-raise-type-error %this "not a Set" this))))
+
+;*---------------------------------------------------------------------*/
+;*    js-set-has ...                                                   */
+;*    -------------------------------------------------------------    */
+;*    https://www.ecma-international.org/ecma-262/6.0/                 */
+;*       #sec-set.prototype.has                                        */
+;*---------------------------------------------------------------------*/
+(define (js-set-has %this)
+   (lambda (this key)
+      (if (isa? this JsMap)
+          (with-access::JsMap this (mapdata)
+             (fixnum? (hashtable-get mapdata key)))
+          (js-raise-type-error %this "not a Set" this))))
+
+
+   
+   
+  
---
+++ test/hopjs/noserv/es6-weakset.js
@@ -0,0 +1,223 @@
+/*=====================================================================*/
+/*    .../prgm/project/hop/3.2.x/test/hopjs/noserv/es6-weakset.js      */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Mon Feb 25 11:33:29 2019                          */
+/*    Last change :  Wed Feb 27 08:10:53 2019 (serrano)                */
+/*    Copyright   :  2019 Manuel Serrano                               */
+/*    -------------------------------------------------------------    */
+/*    Testing ECMAScript 2016 WEAKMAP object.                          */
+/*=====================================================================*/
+"use strict";
+"use hopscript";
+
+const assert = require( "assert" );
+
+/*---------------------------------------------------------------------*/
+/*    misc                                                             */
+/*---------------------------------------------------------------------*/
+function misca() {
+   return true;
+}
+
+function miscb() {
+   return true;
+}
+
+console.log( "misc" );
+console.log( "   misca()"); assert.ok( misca(), "misca" );
+console.log( "   miscb()"); assert.ok( miscb(), "miscb" );
+
+/*---------------------------------------------------------------------*/
+/*    mdn                                                              */
+/*---------------------------------------------------------------------*/
+function mdna() {
+   var ws = new WeakSet();
+   var foo = {};
+   var bar = {};
+
+   ws.add(foo);
+   ws.add(bar);
+   
+   if(!ws.has(foo)) return false;    // true
+   if(!ws.has(bar)) return false;    // true
+
+   ws.delete(foo); // removes foo from the set
+   return !ws.has(foo);
+}
+
+function mdnb() {
+   const weakset1 = new WeakSet();
+   const object1 = {};
+
+   weakset1.add(object1);
+   if(!weakset1.has(object1)) return false;
+
+   try {
+      weakset1.add(1); // raise an error because not an object
+      return false;
+   } catch(error) {
+      return true;
+   }
+}
+
+function mdnc() {
+   const weakset1 = new WeakSet();
+   const object1 = {};
+
+   weakset1.add(object1);
+   
+   if(!weakset1.has(object1)) return false;
+
+   weakset1.delete(object1);
+
+   return !weakset1.has(object1);
+}
+		   
+function mdnd() {
+   const weakset1 = new WeakSet();
+   const object1 = {};
+   const object2 = {};
+
+   weakset1.add(object1);
+
+   if(!weakset1.has(object1)) return false;
+   return !weakset1.has(object2);
+}
+
+console.log( "mdn" );
+console.log( "   mdna()"); assert.ok( mdna(), "mdna" );
+console.log( "   mdnb()"); assert.ok( mdnb(), "mdnb" );
+console.log( "   mdnc()"); assert.ok( mdnc(), "mdnc" );
+console.log( "   mdnd()"); assert.ok( mdnd(), "mdnd" );
+
+/*---------------------------------------------------------------------*/
+/*    kangax ...                                                       */
+/*---------------------------------------------------------------------*/
+function __createIterableObject(arr, methods) {
+   methods = methods || {};
+   if( typeof Symbol !== 'function' || !Symbol.iterator ) {
+      return {};
+   }
+   arr.length++;
+   var iterator = {
+      next: function() {
+         return { value: arr.shift(), done: arr.length <= 0 };
+      },
+      'return': methods[ 'return' ],
+      'throw': methods[ 'throw' ]
+   };
+   var iterable = {};
+   iterable[ Symbol.iterator ] = function(){ return iterator; }
+   return iterable;
+}
+
+function kangaxa() {
+   var obj1 = {};
+   var weakset = new WeakSet();
+
+   weakset.add(obj1);
+   weakset.add(obj1);
+
+   return weakset.has(obj1);
+}
+
+function kangaxb() {
+   var obj1 = {}, obj2 = {};
+   var weakset = new WeakSet([obj1, obj2]);
+
+   return weakset.has(obj1) && weakset.has(obj2);
+}
+
+function kangaxc() {
+   new WeakSet();
+   try {
+      WeakSet();
+      return false;
+   } catch(e) {
+      return true;
+   }
+}
+
+function kangaxd() {
+   new WeakSet(null);
+   return true;
+}
+
+function kangaxe() {
+   var passed = false;
+   var _add = WeakSet.prototype.add;
+
+   WeakSet.prototype.add = function(v) {
+      passed = true;
+   };
+
+   new WeakSet([ { } ]);
+   WeakSet.prototype.add = _add;
+
+   return passed;
+}
+
+function kangaxf() {
+   var closed = false;
+   var iter = __createIterableObject([1, 2, 3], {
+      'return': function(){ closed = true; return {}; }
+   });
+   try {
+      new WeakSet(iter);
+   } catch(e){}
+   return closed;
+}
+
+function kangaxg() {
+   var weakset = new WeakSet();
+   var obj = {};
+   return weakset.add(obj) === weakset;
+}
+
+function kangaxh() {
+   return typeof WeakSet.prototype.delete === "function";
+}
+
+function kangaxi() {
+   if (!("clear" in WeakSet.prototype)) {
+      return true;
+   }
+   var s = new WeakSet();
+   var key = {};
+   s.add(key);
+   s.clear();
+   return s.has(key);
+}
+
+function kangaxj() {
+   var s = new WeakSet;
+   return s.has(1) === false
+      && s.delete(1) === false;
+}
+
+function kangaxk() {
+   new WeakSet();
+   var obj = {};
+   try {
+      WeakSet.prototype.has(obj);
+   }
+   catch(e) {
+      return true;
+   }
+}
+
+console.log( "kangax" );
+console.log( "   kangaxa()"); assert.ok( kangaxa(), "kangaxa" );
+console.log( "   kangaxb()"); assert.ok( kangaxb(), "kangaxb" );
+console.log( "   kangaxc()"); assert.ok( kangaxc(), "kangaxc" );
+console.log( "   kangaxd()"); assert.ok( kangaxd(), "kangaxd" );
+console.log( "   kangaxe()"); assert.ok( kangaxe(), "kangaxe" );
+console.log( "   kangaxf()"); assert.ok( kangaxf(), "kangaxf" );
+console.log( "   kangaxg()"); assert.ok( kangaxg(), "kangaxg" );
+console.log( "   kangaxh()"); assert.ok( kangaxh(), "kangaxh" );
+console.log( "   kangaxi()"); assert.ok( kangaxi(), "kangaxi" );
+console.log( "   kangaxj()"); assert.ok( kangaxj(), "kangaxj" );
+console.log( "   kangaxk()"); assert.ok( kangaxk(), "kangaxk" );
+
+
