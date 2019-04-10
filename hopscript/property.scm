9ff5dd1e5746eb459c6a54833d7b251b311b97d4
Manuel Serrano <Manuel.Serrano@inria.fr>
2019-04-10 09:02:46 +0200

    Builtin method optimization

--- hopscript/property.scm
+++ hopscript/property.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Fri Oct 25 07:05:26 2013                          */
-;*    Last change :  Wed Apr 10 07:39:08 2019 (serrano)                */
+;*    Last change :  Wed Apr 10 08:34:54 2019 (serrano)                */
 ;*    Copyright   :  2013-19 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    JavaScript property handling (getting, setting, defining and     */
@@ -1717,7 +1717,8 @@
 		   throw::bool %this::JsGlobalObject
 		   cache::JsPropertyCache
 		   #!optional (point -1) (cspecs '()))
-   (tprint "JS-OBJECT-GET-NAME/CACHE-MISS obj=" (typeof obj) " name=" name)
+   (tprint "JS-OBJECT-GET-NAME/CACHE-MISS obj=" (typeof obj) " name=" name
+      " " point)
    (js-object-get-lookup obj name throw %this cache point cspecs))
 
 ;*---------------------------------------------------------------------*/
--- js2scheme/scheme-call.scm
+++ js2scheme/scheme-call.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Sun Mar 25 07:00:50 2018                          */
-;*    Last change :  Sat Apr  6 16:32:42 2019 (serrano)                */
+;*    Last change :  Wed Apr 10 08:42:46 2019 (serrano)                */
 ;*    Copyright   :  2018-19 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Scheme code generation of JavaScript function calls              */
@@ -193,7 +193,7 @@
    (let ((array (config-get conf :array)))
       (if (isa? array J2SDeclExtern)
 	  (with-access::J2SDeclExtern array (usage)
-	     (only-usage? '(new init call) usage))
+	     (only-usage? '(new init call get) usage))
 	  #t)))
 
 ;*---------------------------------------------------------------------*/
@@ -203,7 +203,7 @@
    (let ((string (config-get conf :string)))
       (if (isa? string J2SDeclExtern)
 	  (with-access::J2SDeclExtern string (usage)
-	     (only-usage? '(new init call) usage))
+	     (only-usage? '(new init call get) usage))
 	  #t)))
 
 ;*---------------------------------------------------------------------*/
