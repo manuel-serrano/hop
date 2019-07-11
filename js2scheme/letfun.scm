2f1cd7c1a74719937acd373c91c4dee6eb044585
Manuel Serrano <Manuel.Serrano@inria.fr>
2019-07-11 06:49:41 +0200

    .

---
+++ js2scheme/letfun.scm
@@ -0,0 +1,170 @@
+;*=====================================================================*/
+;*    serrano/prgm/project/hop/hop/js2scheme/letfun.scm                */
+;*    -------------------------------------------------------------    */
+;*    Author      :  Manuel Serrano                                    */
+;*    Creation    :  Sun Jun 28 06:35:14 2015                          */
+;*    Last change :  Wed Jul 10 20:00:04 2019 (serrano)                */
+;*    Copyright   :  2015-19 Manuel Serrano                            */
+;*    -------------------------------------------------------------    */
+;*    Let function optimisation                                        */
+;*    -------------------------------------------------------------    */
+;*    Rewrite the following pattern:                                   */
+;*       var f = function() { ... }                                    */
+;*    info                                                             */
+;*       function f() { ... }                                          */
+;*=====================================================================*/
+
+;*---------------------------------------------------------------------*/
+;*    The module                                                       */
+;*---------------------------------------------------------------------*/
+(module __js2scheme_letfun
+
+   (include "ast.sch")
+   
+   (import __js2scheme_ast
+	   __js2scheme_dump
+	   __js2scheme_utils
+	   __js2scheme_compile
+	   __js2scheme_stage
+	   __js2scheme_lexer
+	   __js2scheme_alpha)
+
+   (export j2s-letfun-stage))
+
+;*---------------------------------------------------------------------*/
+;*    j2s-letun-stage ...                                              */
+;*---------------------------------------------------------------------*/
+(define j2s-letfun-stage
+   (instantiate::J2SStageProc
+      (name "letfun")
+      (comment "Implicit function declarations")
+      (proc j2s-letfun)
+      (optional 2)))
+
+;*---------------------------------------------------------------------*/
+;*    j2s-letfun ...                                                   */
+;*---------------------------------------------------------------------*/
+(define (j2s-letfun this args)
+   (when (isa? this J2SProgram)
+      (letfun! this args)))
+
+;*---------------------------------------------------------------------*/
+;*    letfun! ...                                                      */
+;*---------------------------------------------------------------------*/
+(define-walk-method (letfun! this::J2SNode args)
+   (call-default-walker))
+
+;*---------------------------------------------------------------------*/
+;*    letfun! ::J2SFun ...                                             */
+;*---------------------------------------------------------------------*/
+(define-walk-method (letfun! this::J2SFun args)
+   (call-default-walker)
+   (with-access::J2SFun this (body)
+      (if (isa? body J2SBlock)
+	  (multiple-value-bind (vars nodes)
+	     (block-collect-vars body)
+	     (multiple-value-bind (inits rest)
+		(nodes-collect-inits nodes)
+		(let ((inits (filter (lambda (i) (init-fun? i vars)) inits)))
+		   (when (pair? inits)
+		      (let* ((odecls (map decloinit inits))
+			     (ndecls (map declninit inits))
+			     (noinits (filter (lambda (v)
+						 (not (memq v ndecls)))
+					 vars))
+			     (nblock (duplicate::J2SBlock body
+					(nodes rest))))
+			 (set! body 
+			    (duplicate::J2SBlock body
+			       (nodes (append noinits
+					 ndecls
+					 (list (j2s-alpha nblock
+						  odecls
+						  ndecls)))))))))))))
+   this)
+
+;*---------------------------------------------------------------------*/
+;*    decloinit ...                                                    */
+;*---------------------------------------------------------------------*/
+(define (decloinit this::J2SInit)
+   (with-access::J2SInit this (lhs rhs)
+      (with-access::J2SRef lhs (decl)
+	 decl)))
+
+;*---------------------------------------------------------------------*/
+;*    declninit ...                                                    */
+;*---------------------------------------------------------------------*/
+(define (declninit this::J2SInit)
+   (with-access::J2SInit this (lhs rhs)
+      (with-access::J2SRef lhs (decl)
+	 (if (isa? rhs J2SFun)
+	     (with-access::J2SDecl decl (id writable scope usage binder utype vtype loc)
+		(instantiate::J2SDeclFun
+		   (loc loc)
+		   (id id)
+		   (writable writable)
+		   (scope scope)
+		   (usage usage)
+		   (binder binder)
+		   (utype utype)
+		   (vtype vtype)
+		   (val rhs)))
+	     (duplicate::J2SDeclInit decl
+		(val rhs))))))
+
+;*---------------------------------------------------------------------*/
+;*    init-fun? ...                                                    */
+;*---------------------------------------------------------------------*/
+(define (init-fun? this::J2SInit vars)
+   (with-access::J2SInit this (lhs rhs)
+      (when (isa? rhs J2SFun)
+	 (with-access::J2SRef lhs (decl)
+	    (memq decl vars)))))
+
+;*---------------------------------------------------------------------*/
+;*    block-collect-vars ...                                           */
+;*---------------------------------------------------------------------*/
+(define (block-collect-vars node::J2SBlock)
+   (with-access::J2SBlock node (nodes)
+      (let loop ((nodes nodes)
+		 (vars '()))
+	 (cond
+	    ((null? nodes)
+	     (values (reverse! vars) '()))
+	    ((isa? (car nodes) J2SDecl)
+	     (loop (cdr nodes) (cons (car nodes) vars)))
+	    (else
+	     (values (reverse! vars) nodes))))))
+
+;*---------------------------------------------------------------------*/
+;*    nodes-collect-inits ...                                          */
+;*---------------------------------------------------------------------*/
+(define (nodes-collect-inits nodes::pair-nil)
+   
+   (define (init node)
+      (when (isa? node J2SStmtExpr)
+	 (with-access::J2SStmtExpr node (expr)
+	    (when (isa? expr J2SInit)
+	       (with-access::J2SInit expr (rhs)
+		  (when (or (isa? rhs J2SFun) (isa? rhs J2SLiteral))
+		     expr))))))
+   
+   (let loop ((nodes nodes)
+	      (inits '()))
+      (cond
+	 ((null? nodes)
+	  (values (reverse! inits) '()))
+	 ((isa? (car nodes) J2SLetBlock)
+	  (values (reverse! inits) nodes))
+	 ((isa? (car nodes) J2SSeq)
+	  (with-access::J2SSeq (car nodes) ((bnodes nodes))
+	     (loop (append bnodes (cdr nodes)) inits)))
+	 ((init (car nodes))
+	  =>
+	  (lambda (init)
+	     (loop (cdr nodes) (cons init inits))))
+	 (else
+	  (values (reverse! inits) nodes)))))
+
+	 
+   
---
+++ js2scheme/objinit.scm
@@ -0,0 +1,88 @@
+;*=====================================================================*/
+;*    serrano/prgm/project/hop/hop/js2scheme/objinit.scm               */
+;*    -------------------------------------------------------------    */
+;*    Author      :  Manuel Serrano                                    */
+;*    Creation    :  Wed Jul 10 14:08:36 2019                          */
+;*    Last change :  Thu Jul 11 06:49:21 2019 (serrano)                */
+;*    Copyright   :  2019 Manuel Serrano                               */
+;*    -------------------------------------------------------------    */
+;*    An optimization that annotated read-only literal objects         */
+;*    (i.e., literal objects that are only used in get position).      */
+;*                                                                     */
+;*    This optimization is not sophisticated, it only checks           */
+;*    literal objects assigned to get-only variables.                  */
+;*=====================================================================*/
+
+;*---------------------------------------------------------------------*/
+;*    The module                                                       */
+;*---------------------------------------------------------------------*/
+(module __js2scheme_objinit
+
+   (include "ast.sch")
+   
+   (import __js2scheme_ast
+	   __js2scheme_dump
+	   __js2scheme_compile
+	   __js2scheme_stage
+	   __js2scheme_syntax
+	   __js2scheme_utils
+	   __js2scheme_type-hint)
+
+   (export j2s-objinit-stage))
+
+;*---------------------------------------------------------------------*/
+;*    j2s-objinit-stage ...                                            */
+;*---------------------------------------------------------------------*/
+(define j2s-objinit-stage
+   (instantiate::J2SStageProc
+      (name "objinit")
+      (comment "Annotate read-only literal objects")
+      (proc j2s-objinit!)))
+
+;*---------------------------------------------------------------------*/
+;*    j2s-objinit! ...                                                 */
+;*---------------------------------------------------------------------*/
+(define (j2s-objinit! this args)
+   (when (isa? this J2SProgram)
+     (objinit! this args))
+   this)
+
+;*---------------------------------------------------------------------*/
+;*    objinit! ::J2SNode ...                                           */
+;*---------------------------------------------------------------------*/
+(define-walk-method (objinit! this::J2SNode args)
+   (call-default-walker))
+
+;*---------------------------------------------------------------------*/
+;*    objinit! ::J2SAssig ...                                          */
+;*---------------------------------------------------------------------*/
+(define-walk-method (objinit! this::J2SAssig arg)
+   (with-access::J2SAssig this (lhs rhs)
+      (when (and (isa? lhs J2SRef) (get-only-ref? lhs) (isa? rhs J2SObjInit))
+	 (with-access::J2SObjInit rhs (ronly)
+	    (set! ronly #t)))
+      (call-default-walker)))
+
+;*---------------------------------------------------------------------*/
+;*    objinit! ::J2SDeclInit ...                                       */
+;*---------------------------------------------------------------------*/
+(define-walk-method (objinit! this::J2SDeclInit arg)
+   (with-access::J2SDeclInit this (val)
+      (when (and (get-only-decl? this) (isa? val J2SObjInit))
+	 (with-access::J2SObjInit val (ronly)
+	    (set! ronly #t))))
+   (call-default-walker))
+
+;*---------------------------------------------------------------------*/
+;*    get-only-ref? ...                                                */
+;*---------------------------------------------------------------------*/
+(define (get-only-ref? ref)
+   (with-access::J2SRef ref (decl)
+      (get-only-decl? decl)))
+
+;*---------------------------------------------------------------------*/
+;*    get-only-decl? ...                                               */
+;*---------------------------------------------------------------------*/
+(define (get-only-decl? decl)
+   (decl-only-usage? decl '(get init assig)))
+
