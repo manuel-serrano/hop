4646d88db5de539626f22821dbae06fac7ad2fa2
Manuel Serrano <Manuel.Serrano@inria.fr>
2019-08-29 09:18:07 +0200

    Remove debug message

--- runtime/js_comp.scm
+++ runtime/js_comp.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Tue Jul 19 15:55:02 2005                          */
-;*    Last change :  Thu Jul 18 06:27:57 2019 (serrano)                */
+;*    Last change :  Mon Aug 26 19:59:08 2019 (serrano)                */
 ;*    Copyright   :  2005-19 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    JS compilation tools                                             */
@@ -101,7 +101,6 @@
 ;*    hop->javascript ...                                              */
 ;*---------------------------------------------------------------------*/
 (define-generic (hop->javascript obj op::output-port compile isexpr _)
-       (tprint "hop->javascript" obj)
    (cond
       ((procedure? obj)
        (let ((attr (procedure-attr obj)))
