5323a18d70454dc346f1f5ec3199c05b1b01a882
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-01-17 10:27:40 +0100

    arithmetic32 fix

--- hopscript/arithmetic32.scm
+++ hopscript/arithmetic32.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Mon Dec  4 19:36:39 2017                          */
-;*    Last change :  Fri Jan 17 08:31:35 2020 (serrano)                */
+;*    Last change :  Fri Jan 17 10:07:49 2020 (serrano)                */
 ;*    Copyright   :  2017-20 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Arithmetic operations on 32bit and nan64 platforms               */
@@ -593,9 +593,9 @@
 	     (else
 	      (pragma::bint "BINT($1)" res)))))
       ((and bigloo-c (config have-overflow #t))
-       (let ((res::long 0))
+       (let ((res::int 0))
 	  (cond
-	     ((pragma::bool "__builtin_smull_overflow(CINT($1), CINT($2), &$3)"
+	     ((pragma::bool "__builtin_smul_overflow((int)(CINT($1)), (int)(CINT($2)), &$3)"
 		 x y (pragma res))
 	      (pragma::real "DOUBLE_TO_REAL(((double)(CINT(($1))))*((double)(CINT($2))))"
 		 x y))
