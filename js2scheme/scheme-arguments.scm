229bef7dd992a767b39ee7d9651cd70d9eea6696
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-04-16 18:31:29 +0200

    Minor fix

--- js2scheme/scheme-arguments.scm
+++ js2scheme/scheme-arguments.scm
@@ -3,7 +3,7 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Thu Oct  5 05:47:06 2017                          */
-;*    Last change :  Thu Mar 12 20:38:41 2020 (serrano)                */
+;*    Last change :  Thu Apr 16 18:30:46 2020 (serrano)                */
 ;*    Copyright   :  2017-20 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Scheme code generation of JavaScript arguments functions.        */
@@ -14,7 +14,7 @@
 ;*---------------------------------------------------------------------*/
 (module __js2scheme_scheme-arguments
 
-   (include "ast.sch")
+   (include "ast.sch" "context.sch")
    
    (import __js2scheme_ast
 	   __js2scheme_dump
@@ -29,9 +29,9 @@
 	   __js2scheme_scheme-utils
 	   __js2scheme_scheme-fun)
 
-   (export (j2s-rest-ref ::J2SAccess mode return conf)
-	   (j2s-arguments-ref ::J2SAccess mode return conf)
-	   (j2s-arguments-set! ::J2SAssig mode return conf)
+   (export (j2s-rest-ref ::J2SAccess mode return ::struct)
+	   (j2s-arguments-ref ::J2SAccess mode return ::struct)
+	   (j2s-arguments-set! ::J2SAssig mode return ::struct)
 	   (j2s-ref-arguments-lazy?::bool ::J2SRef)
 	   (j2s-ref-arguments-argid::symbol ::J2SRef)
 	   (j2s-ref-arguments-mode::symbol ::J2SRef)))
@@ -39,7 +39,7 @@
 ;*---------------------------------------------------------------------*/
 ;*    j2s-rest-ref ...                                                 */
 ;*---------------------------------------------------------------------*/
-(define (j2s-rest-ref this::J2SAccess mode return conf)
+(define (j2s-rest-ref this::J2SAccess mode return ctx)
    (with-access::J2SAccess this (obj field type)
       (when (isa? obj J2SRef)
 	 (with-access::J2SRef obj (decl)
@@ -51,25 +51,25 @@
 		      (cond
 			 ((eq? (j2s-vtype field) 'uint32)
 			  `(js-rest-vector-index-ref 
-			      ,(j2s-scheme obj mode return conf)
-			      ,(j2s-scheme field mode return conf)))
+			      ,(j2s-scheme obj mode return ctx)
+			      ,(j2s-scheme field mode return ctx)))
 			 ((eq? (j2s-vtype field) 'int32)
 			  `(js-rest-vector-ref 
-			      ,(j2s-scheme obj mode return conf)
-			      (int32->fixnum ,(j2s-scheme field mode return conf))))
+			      ,(j2s-scheme obj mode return ctx)
+			      (int32->fixnum ,(j2s-scheme field mode return ctx))))
 			 (else
 			  `(js-rest-ref
-			      ,(j2s-scheme obj mode return conf)
-			      ,(j2s-scheme field mode return conf)))))
+			      ,(j2s-scheme obj mode return ctx)
+			      ,(j2s-scheme field mode return ctx)))))
 		     ((j2s-field-length? field)
-		      `(vector-length ,(j2s-scheme obj mode return conf)))
+		      `(vector-length ,(j2s-scheme obj mode return ctx)))
 		     (else
 		      #f))))))))
 
 ;*---------------------------------------------------------------------*/
 ;*    j2s-arguments-ref ...                                            */
 ;*---------------------------------------------------------------------*/
-(define (j2s-arguments-ref this::J2SAccess mode return conf)
+(define (j2s-arguments-ref this::J2SAccess mode return ctx)
    (with-access::J2SAccess this (obj field type)
       (cond
 	 ((maybe-number? field)
@@ -78,78 +78,78 @@
 	      (if (and (isa? obj J2SRef) (j2s-ref-arguments-lazy? obj))
 		  (let ((argid (j2s-ref-arguments-argid obj)))
 		     `(js-arguments-vector-index-ref ,argid
-		       ,(j2s-scheme obj mode return conf)
-		       ,(j2s-scheme field mode return conf)
+		       ,(j2s-scheme obj mode return ctx)
+		       ,(j2s-scheme field mode return ctx)
 		       %this))
-		  `(js-arguments-index-ref ,(j2s-scheme obj mode return conf)
-		      ,(j2s-scheme field mode return conf)
+		  `(js-arguments-index-ref ,(j2s-scheme obj mode return ctx)
+		      ,(j2s-scheme field mode return ctx)
 		      %this)))
 	     ((eq? (j2s-vtype field) 'int32)
 	      (let ((argid (j2s-ref-arguments-argid obj)))
 		 `(js-arguments-vector-ref ,argid
-		   ,(j2s-scheme obj mode return conf)
-		   (int32->fixnum ,(j2s-scheme field mode return conf))
+		   ,(j2s-scheme obj mode return ctx)
+		   (int32->fixnum ,(j2s-scheme field mode return ctx))
 		   %this)))
 	     ((and (isa? obj J2SRef) (j2s-ref-arguments-lazy? obj))
 	      (let ((argid (j2s-ref-arguments-argid obj)))
 		 `(js-arguments-vector-ref ,argid
-		   ,(j2s-scheme obj mode return conf)
-		   ,(j2s-scheme field mode return conf)
+		   ,(j2s-scheme obj mode return ctx)
+		   ,(j2s-scheme field mode return ctx)
 		   %this)))
 	     (else
-	      `(js-arguments-ref ,(j2s-scheme obj mode return conf)
-		  ,(j2s-scheme field mode return conf)
+	      `(js-arguments-ref ,(j2s-scheme obj mode return ctx)
+		  ,(j2s-scheme field mode return ctx)
 		  %this))))
 	 ((j2s-field-length? field)
 	  (if (and (isa? obj J2SRef) (j2s-ref-arguments-lazy? obj))
-	      `(if (object? ,(j2s-scheme obj mode return conf))
+	      `(if (object? ,(j2s-scheme obj mode return ctx))
 		   (js-arguments-length
-		      ,(j2s-scheme obj mode return conf) %this)
+		      ,(j2s-scheme obj mode return ctx) %this)
 		   (vector-length ,(j2s-ref-arguments-argid obj)))
 	      `(js-arguments-length
-		  ,(j2s-scheme obj mode return conf) %this)))
+		  ,(j2s-scheme obj mode return ctx) %this)))
 	 (else
 	  #f))))
 
 ;*---------------------------------------------------------------------*/
 ;*    j2s-arguments-set! ...                                           */
 ;*---------------------------------------------------------------------*/
-(define (j2s-arguments-set! this::J2SAssig mode return conf)
+(define (j2s-arguments-set! this::J2SAssig mode return ctx)
    (with-access::J2SAssig this (lhs rhs)
       (with-access::J2SAccess lhs (obj field cache cspecs loc)
-	 (if (config-get conf :optim-arguments)
+	 (if (context-get ctx :optim-arguments)
 	     (if (eq? (j2s-vtype field) 'uint32)
 		 (if (and (isa? obj J2SRef) (j2s-ref-arguments-lazy? obj))
 		     (let ((argid (j2s-ref-arguments-argid obj)))
 			`(js-arguments-vector-index-set! ,argid
-			    ,(j2s-scheme obj mode return conf)
-			    ,(j2s-scheme field mode return conf)
-			    ,(j2s-scheme rhs mode return conf)
+			    ,(j2s-scheme obj mode return ctx)
+			    ,(j2s-scheme field mode return ctx)
+			    ,(j2s-scheme rhs mode return ctx)
 			    %this))
-		     `(js-arguments-index-ref ,(j2s-scheme obj mode return conf)
-			 ,(j2s-scheme field mode return conf)
-			 ,(j2s-scheme rhs mode return conf)
+		     `(js-arguments-index-ref ,(j2s-scheme obj mode return ctx)
+			 ,(j2s-scheme field mode return ctx)
+			 ,(j2s-scheme rhs mode return ctx)
 			 %this))
-		 (j2s-put! loc (j2s-scheme obj mode return conf) field
-		    (typeof-this obj conf)
-		    (j2s-scheme field mode return conf)
+		 (j2s-put! loc (j2s-scheme obj mode return ctx) field
+		    (typeof-this obj ctx)
+		    (j2s-scheme field mode return ctx)
 		    (j2s-vtype field)
-		    (j2s-scheme rhs mode return conf)
+		    (j2s-scheme rhs mode return ctx)
 		    (j2s-vtype rhs)
 		    (strict-mode? mode)
-		    conf
+		    ctx
 		    cache
 		    #f
 		    :cspecs cspecs
 		    :cachefun #f))
-	     (j2s-put! loc (j2s-scheme obj mode return conf) field
-		(typeof-this obj conf)
-		(j2s-scheme field mode return conf)
+	     (j2s-put! loc (j2s-scheme obj mode return ctx) field
+		(typeof-this obj ctx)
+		(j2s-scheme field mode return ctx)
 		(j2s-vtype field)
-		(j2s-scheme rhs mode return conf)
+		(j2s-scheme rhs mode return ctx)
 		(j2s-vtype rhs)
 		(strict-mode? mode)
-		conf
+		ctx
 		cache
 		#f
 		:cspecs cspecs
