5addb1fd21d55e2c18e1afa42e506fa9764bfc01
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-05-14 12:05:01 +0200

    Android

--- arch/android/buildapk.sh
+++ arch/android/buildapk.sh
@@ -4,7 +4,7 @@
 #*    -------------------------------------------------------------    */
 #*    Author      :  manuel serrano                                    */
 #*    Creation    :  Wed May 13 18:51:59 2020                          */
-#*    Last change :  Wed May 13 19:10:19 2020 (serrano)                */
+#*    Last change :  Thu May 14 08:45:08 2020 (serrano)                */
 #*    Copyright   :  2020 manuel serrano                               */
 #*    -------------------------------------------------------------    */
 #*    build the Android APK after ndk-build                            */
@@ -24,6 +24,8 @@ ANDROIDCP=
 
 javac=javac
 jarsigner=jarsigner
+
+androidkeystore=$HOME/.android/debug.keystore
 
 #*---------------------------------------------------------------------*/
 #*    argument parsing                                                 */
@@ -48,12 +50,15 @@ while : ; do
     --androidcp=*)
       ANDROIDCP="`echo $1 | sed 's/^[^=]*=//'`";;
 
+    --androidkeystore=*)
+      androidkeystore="`echo $1 | sed 's/^[^=]*=//'`";;
+
     --javac=*)
       javac="`echo $1 | sed 's/^[^=]*=//'`";;
 
     --jarsigner=*)
       jarsigner="`echo $1 | sed 's/^[^=]*=//'`";;
-
+    
     *)
       apkname=$1;;
   esac
@@ -61,11 +66,11 @@ while : ; do
 done
 
 if [ "$ANDROIDBUILDTOOLSVERSION " = " " ]; then
-  ANDROIDBUILDTOOLSVERSION=`ls $ANDROIDSDK/build-tools | tail -n 1 | sed 's|/||'`
+  ANDROIDBUILDTOOLSVERSION=`ls $ANDROIDSDK/build-tools | tail -n 1 | sed 's|/||'` || exit 1
 fi
 
 if [ "$ANDROIDPLATFORM " = " " ]; then
-  ANDROIDPLATFORM=`ls $ANDROIDSDK/platform | tail -n 1 | sed 's|/||'`
+  ANDROIDPLATFORM=`ls $ANDROIDSDK/platforms | tail -n 1 | sed 's|/||'` || exit 1
 fi
 
 if [ "$ANDROIDCP " = " " ]; then
@@ -96,14 +101,16 @@ rm -rf lib
 cp -r libs lib
 
 for p in `find lib -type f -print`; do
-  $AAPT add $APKNAME.apk.unaligned $p
+  $AAPT add $apkname.apk.unaligned $p
 done
 
 for p in `find assets -type f -print`; do
-  $AAPT add $APKNAME.apk.unaligned $p
+  $AAPT add $apkname.apk.unaligned $p
 done
 
-jarsigner -keystore ~/.android/debug.keystore -storepass 'android' $apkname.apk.unaligned androiddebugkey
+jarsigner -keystore $androidkeystore -storepass 'android' $apkname.apk.unaligned androiddebugkey
 
 /bin/rm -f $apkname.apk
 $ZIPALIGN -f 4 $apkname.apk.unaligned $apkname.apk
+
+/bin/rm -f $apkname.apk.unaligned
