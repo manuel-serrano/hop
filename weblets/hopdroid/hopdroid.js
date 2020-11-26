4a07563b101f9efea2fe85b0f64a3232305995bc
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-11-26 19:09:17 +0100

    Android

---
+++ weblets/hopdroid/hopdroid.js
@@ -0,0 +1,153 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/work/hopdroid/hopdroid.js               */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Wed Nov 25 07:08:20 2020                          */
+/*    Last change :  Thu Nov 26 16:42:55 2020 (serrano)                */
+/*    Copyright   :  2020 Manuel Serrano                               */
+/*    -------------------------------------------------------------    */
+/*    Hopdroid application                                             */
+/*=====================================================================*/
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+import * as sp from hop.spage;
+
+import { NAVTITLE } from './xml.js';
+import { APPS } from './apps.js';
+import { SYSTEM } from './system.js';
+import { ABOUT } from './about.js';
+
+/*---------------------------------------------------------------------*/
+/*    hopdroid                                                         */
+/*---------------------------------------------------------------------*/
+service hopdroid( o ) {
+   return <html>
+     
+     <head>
+       <meta name="viewport" 
+	     content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+       <link rel="manifest" href=${manifest()}/>
+       <link href=${sp.css} rel="stylesheet" type="text/css"/>
+       <link href=${require.resolve( "./hopdroid.hss" )} rel="stylesheet" type="text/css"/>
+       <link href=${require.resolve( "./apps.hss" )} rel="stylesheet" type="text/css"/>
+       <link href=${require.resolve( "./system.hss" )} rel="stylesheet" type="text/css"/>
+       <link href=${require.resolve( "./about.hss" )} rel="stylesheet" type="text/css"/>
+       <script src=${sp.script} type="application/x-javascript"/>
+     </head>
+     
+     <body class="hopdroid">
+       <sp.spage id="spage">
+	 <sp.sphead class="main">
+	   <navtitle spageid="spage">Hop</navtitle>
+	 </sp.sphead>
+
+	 <div class="spage-block">
+	   <privacy/>
+	 </div>
+	 
+	 <div class="spage-block">
+	   <webdav/>
+	   <apps/>
+	 </div>
+
+	 <div class="spage-block">
+	   <system/>
+	   <about/>
+	 </div>
+	 
+	 <div class="spage-block">
+	 </div>
+	 
+       </sp.spage>
+     </body>
+   </html>;
+}
+
+function PRIVACY() {
+   return <sp.sptab svc=${webdav}>
+     <sp.sptabhead>
+       <nav class="sptabhead unselected">
+	 <ul>
+	   <li>
+	     <div class="icon">
+	       <svg:img class="privacy-icon" width="16px" height="16px" 
+			src=${require.resolve( "./icons/shield-lock.svg" )}/>
+	     </div>
+	   </li>
+	   <li>
+	     <div class="title">
+	       <div>Security</div>
+	       <div class="subtitle">authentication, permissions</div>
+	     </div>
+	   </li>
+	 </ul>
+       </nav>
+       <navtitle spageid="spage" class="sphead selected" arrow="&#8672;">Privacy</navtitle>
+     </sp.sptabhead>
+   </sp.sptab>
+}
+
+function WEBDAV() {
+   return <sp.sptab svc=${webdav}>
+     <sp.sptabhead>
+       <nav class="sptabhead unselected">
+	 <ul>
+	   <li>
+	     <div class="icon">
+	       <svg:img class="hdd-icon" width="16px" height="16px" 
+			src=${require.resolve( "./icons/hdd-network.svg" )}/>
+	     </div>
+	   </li>
+	   <li>
+	     <div class="title">
+	       <div>Webdav configuration</div>
+	       <div class="subtitle">hdd sharing</div>
+	     </div>
+	   </li>
+	 </ul>
+       </nav>
+       <navtitle spageid="spage" class="sphead selected" arrow="&#8672;">Hdd Sharing</navtitle>
+     </sp.sptabhead>
+   </sp.sptab>
+}
+   
+service webdav() {
+   return <div>
+     <div class="setting">
+       <div class="icon-sans">
+	 <svg:img class="about-icon" width="16px" height="16px" 
+		  src=${require.resolve( "./icons/hdd.svg" )}/>
+       </div>
+       <div class="text">
+	 Enable webdav
+       </div>
+       <div class="button"
+	    onclick=~{document.getElementById( "webdav-toggle" ).setAttribute( "data-on", "on" )}>
+	 <svg:img id="webdav-toggle" class="icon-toggle" width="24px" height="24px" 
+		  src=${require.resolve( "./icons/toggle2-off.svg" )}/>
+       </div>
+     </div>
+   </div>;
+}
+     
+service manifest() {
+   return hop.HTTPResponseString( 
+      { "charset": hop.charset,
+	"header": {
+	   "Cache-Control": "no-cache",
+	   "Pragma": "no-cache"
+	},
+	"content-type": "application/manifest+json",
+	"bodyp": false,
+	"content-length": -1,
+	"body": `{"short_name": "hopc@${hop.hostname}", 
+		  "name": "hop v${hop.version}, 
+                  "start_url": "/hop/hopdroid", 
+		  "display": "standalone", 
+                  "orientation": "portrait",
+                  "description": "Hop on Android",
+                  "icons": [ { "src": "${require.resolve( './hop.svg' )}", "type": "image/svg" } ]}`
+      } );
+}
+
---
+++ weblets/hopdroid/xml.js
@@ -0,0 +1,41 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/work/hopdroid/xml.js                    */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Wed Nov 25 08:28:29 2020                          */
+/*    Last change :  Wed Nov 25 15:17:26 2020 (serrano)                */
+/*    Copyright   :  2020 Manuel Serrano                               */
+/*    -------------------------------------------------------------    */
+/*    hopdroid xml tags                                                */
+/*=====================================================================*/
+import { NAVTITLE } from './xml.js';
+
+/*---------------------------------------------------------------------*/
+/*    NAVTITLE ...                                                     */
+/*---------------------------------------------------------------------*/
+export function NAVTITLE( attrs, title ) {
+   return <nav class=${attrs.class || "sphead"}>
+     <ul>
+       <li class="nav-back">
+	 ${attrs.arrow ?
+	    <div class="outer-relief">
+	      <button class="nav-back inner-relief"
+		      onclick=~{document.getElementById( ${attrs.spageid} ).pop()}>${attrs.arrow}</button>
+	    </div> 
+	    : <div>
+		<svg:img id="main-logo" class="hop-logo" 
+		       	 width="2ex" height="2ex" 
+		       	 src=${require.resolve( "./hop-sans-style.svg" )}/>
+	      </div>}
+       </li>
+       <li class="nav-title">
+	 ${title}
+       </li>
+       <li class="nav-logo">
+	 <svg:img id="main" class="hop-logo-sans" 
+		  width="2ex" height="2ex" 
+		  src=${require.resolve( "./hop-sans-style.svg" )}/> 
+       </li>
+     </ul>
+   </nav>
+}
