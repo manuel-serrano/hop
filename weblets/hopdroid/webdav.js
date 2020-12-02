dff9b6e13043129ff6c4f041852b08b0d4dc26e8
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-12-02 18:13:49 +0100

    Hopdroid Webdav configuration

---
+++ weblets/hopdroid/webdav.js
@@ -0,0 +1,99 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/hop/weblets/hopdroid/webdav.js          */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Wed Dec  2 16:36:06 2020                          */
+/*    Last change :  Wed Dec  2 16:36:06 2020 (serrano)                */
+/*    Copyright   :  2020 Manuel Serrano                               */
+/*    -------------------------------------------------------------    */
+/*    webdav configuration                                             */
+/*=====================================================================*/
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+import { config } from hop.config;
+import { hopdroid } from hop.hopdroid;
+import * as sp from hop.spage;
+import { NAVTITLE } from './xml.js';
+
+/*---------------------------------------------------------------------*/
+/*    WEBDAV ...                                                       */
+/*---------------------------------------------------------------------*/
+export function WEBDAV() {
+   return <sp.sptab svc=${webdav}>
+     <sp.sptabhead>
+       <nav class="sptabhead unselected">
+	 <ul>
+	   <li>
+	     <div class="icon">
+	       <svg:img class="hdd-icon" width="16px" height="16px" 
+			src=${require.resolve( "./icons/hdd-network.svg" )}/>
+	     </div>
+	   </li>
+	   <li>
+	     <div class="title">
+	       <div>WebDav configuration</div>
+	       <div class="subtitle">storage sharing</div>
+	     </div>
+	   </li>
+	 </ul>
+       </nav>
+       <navtitle spageid="spage" class="sphead selected" arrow="&#8672;">
+	 WebDav configuration
+       </navtitle>
+     </sp.sptabhead>
+   </sp.sptab>
+}
+
+/*---------------------------------------------------------------------*/
+/*    webdav ...                                                       */
+/*---------------------------------------------------------------------*/
+service webdav() {
+   console.log( "wd=", config.enableWebdav );
+   
+   return <div class="webdav-page">
+     <script>
+       function webdavToggle() {
+	  ${webdavToggle}()
+	     .post()
+	     .then( v => 
+		document.getElementById( "webdav-toggle" )
+		       .setAttribute( "data-on", v ? "on" : "off" ) );
+       }
+     </script>
+     
+     <div class="webdav-config">
+       <div class="title">
+	 <svg:img class="icon" width="16px" height="16px" 
+		  src=${require.resolve( "./icons/hdd.svg" )}/>
+       </div>
+       <div class="text">
+	 Toggle WebDav
+	 <div class="comment"> 
+	   Enabling or disabling WebDav is only effective after server restart
+	 </div>
+       </div>
+       <div class="button" onclick=~{ webdavToggle() }>
+	 <svg:img id="webdav-toggle" 
+		  data-on=${config.enableWebdav ? "on" : "off" }
+		  class="icon-toggle" width="24px" height="24px" 
+		  src=${require.resolve( "./icons/toggle2-off.svg" )}/>
+       </div>
+     </div>
+   </div>;
+}
+     
+/*---------------------------------------------------------------------*/
+/*    webdavToggle ...                                                 */
+/*---------------------------------------------------------------------*/
+service webdavToggle() {
+   console.log( "toggle..." );
+   config.enableWebdav = !config.enableWebdav;
+   return config.enableWebdav;
+}
+
+/*---------------------------------------------------------------------*/
+/*    services                                                         */
+/*---------------------------------------------------------------------*/
+webdav.path = "/hop/hopdroid/webdav";
+webdavToggle.path = "/hop/hopdroid/webdav/toggle";
