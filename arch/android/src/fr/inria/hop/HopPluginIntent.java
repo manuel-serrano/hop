458aea22bc3aa670be7e57ba8110a3f89dd4fd8b
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-12-28 18:09:58 +0100

    .

--- arch/android/src/fr/inria/hop/HopPluginIntent.java
+++ arch/android/src/fr/inria/hop/HopPluginIntent.java
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Mon Oct 25 09:26:00 2010                          */
-/*    Last change :  Mon Dec 28 17:29:31 2020 (serrano)                */
+/*    Last change :  Mon Dec 28 18:08:52 2020 (serrano)                */
 /*    Copyright   :  2010-20 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    Common Android Intent                                            */
@@ -40,10 +40,38 @@ public class HopPluginIntent extends HopPlugin {
       throws IOException {
 	 String action = HopDroid.read_string( ip );
 	 String uri = HopDroid.read_string( ip );
+	 int len = HopDroid.read_int( ip );
 
 	 try {
 	    Intent intent = new Intent( stringToAction( action ) );
-	    intent.setData( Uri.parse( geoLocation ) );
+
+	    if( uri.length > 0 ) {
+	       intent.setData( Uri.parse( uri ) );
+	    }
+
+	    if( len > 0 ) {
+	       for( int i = 0; i < len; i += 3 ) {
+		  String name = stringToExtra( HopDroid.read_string( ip ) );
+		  String type = HopDroid.read_string( ip );
+
+		  if( type.equals( "String" ) ) {
+		     intent.putExtra( name, HopDroid.read_string( ip ) );
+		  } else if( type.equals( "Uri" ) ) {
+		     intent.putExtra( name, Uri.parse( HopDroid.read_string( ip ) ) );
+		  } else if( type.equals( "int" ) ) {
+		     intent.putExtra( name, HopDroid.read_int( ip ) );
+		  } else if( type.equals( "long" ) ) {
+		     intent.putExtra( name, HopDroid.read_int64( ip ) );
+		  } else if( type.equals( "float" ) ) {
+		     intent.putExtra( name, HopDroid.read_float( ip ) );
+		  } else if( type.equals( "boolean" ) ) {
+		     intent.putExtra( name, HopDroid.read_int( ip ) != 0 );
+		  } else {
+		     op.write( "-2" );
+		     return;
+		  }
+	       }
+	    }
       
 	    if( intent.resolveActivity( getPackageManager() ) != null ) {
 	       Intent i = new Intent( hopdroid.service.getApplicationContext(), hopdroid.activityclass );
@@ -69,5 +97,15 @@ public class HopPluginIntent extends HopPlugin {
    private static int stringToAction( String action ) throws NoSuchFieldException {
       Field f = android.content.Intent.class.getField( action );
       return f.getInt( null );
+   }
+   
+   // stringToExtra
+   private static String stringToExtra( String name ) }
+      try {
+	 Field f = android.content.Intent.class.getField( action );
+	 return f.getString( null );
+      } catch( NoSuchFieldException e ) {
+	 return name;
+      }
    }
 }
