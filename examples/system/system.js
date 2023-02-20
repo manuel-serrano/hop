e481b119506b67b0fc06d59685f74d00b38dfdab
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-02-20 08:48:12 +0100

    .

--- examples/system/system.js
+++ examples/system/system.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Sun Aug 23 08:07:57 2015                          */
-/*    Last change :  Mon Feb 20 08:43:11 2023 (serrano)                */
+/*    Last change :  Mon Feb 20 08:45:57 2023 (serrano)                */
 /*    Copyright   :  2015-23 Manuel Serrano                            */
 /*    -------------------------------------------------------------    */
 /*    An example of SYSTEM                                             */
@@ -13,10 +13,10 @@
 
 import { systemSync as system } from hop.system;
 
-const statusOrData = system("ls -l /tmp");
+const res = system("ls -l /tmp");
 
-if (typeof statusOrData === "number") {
-   console.log("error", statusOrData);
+if (typeof res === "string") {
+   console.log(res.split("\n"));
 } else {
-   console.log(statusOrData.split());
+   console.log("error", fail.status, fail.data);
 }
