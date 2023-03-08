9090e88f78914113feebe888219caddc4059ef60
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-03-08 12:34:15 +0100

    ReWrite lib configuration

---
+++ hopscript/bglhopscript_rewrite.h
@@ -0,0 +1,36 @@
+/*=====================================================================*/
+/*    .../prgm/project/hop/hop/hopscript/bglhopscript_rewrite.h        */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Fri Feb 11 09:35:38 2022                          */
+/*    Last change :  Wed Mar  8 11:38:53 2023 (serrano)                */
+/*    Copyright   :  2022-23 Manuel Serrano                            */
+/*    -------------------------------------------------------------    */
+/*    Code rewrite (patching) macros.                                  */
+/*=====================================================================*/
+#ifndef BGLHOPSCRIPT_REWRITE_H 
+#define BGLHOPSCRIPT_REWRITE_H
+
+/*---------------------------------------------------------------------*/
+/*    Dynamic code patching                                            */
+/*---------------------------------------------------------------------*/
+#if !defined(MODE_REWRITE_OPCODE)
+#  define HOP_REWRITE_LOCATIONS(n)
+#  define HOP_REWRITE_INIT()
+#  define HOP_REWRITE_CACHE_HIT(n) 
+#  define HOP_REWRITE_CACHE_MISS(n, entry)
+#else
+#  include "RewriteLib.h"
+
+#  define HOP_REWRITE_LOCATIONS(n) \
+   static char *hop_rewrite_locations[n] = { NULL }
+#  define HOP_REWRITE_INIT() \
+     init_rewrite_lib()
+#  define HOP_CACHE_HIT(n) \
+     BINREWRITELIB_EXPAND_LABEL(n)
+#  define HOP_CACHE_MISS(n, entry) \
+     BINREWRITELIB_CACHE_MISS_32(n, &(__bgl_pcache[((long) n)].entry), &hop_rewrite_locations[n])
+#endif
+
+#endif
+
