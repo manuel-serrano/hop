9ca5836f02d1d1d502ee2927a59dd6b10458434d
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-05-06 18:27:10 +0200

    hopsched

---
+++ hopsched/amany_scheduler.scm
@@ -0,0 +1,32 @@
+;*=====================================================================*/
+;*    serrano/prgm/project/hop/2.4.x/src/amany_scheduler.scm           */
+;*    -------------------------------------------------------------    */
+;*    Author      :  Manuel Serrano                                    */
+;*    Creation    :  Tue Feb 26 07:03:15 2008                          */
+;*    Last change :  Fri Mar 29 10:51:07 2013 (serrano)                */
+;*    Copyright   :  2008-13 Manuel Serrano                            */
+;*    -------------------------------------------------------------    */
+;*    Accept-many scheduler                                            */
+;*    -------------------------------------------------------------    */
+;*    A mere variation around the pool-scheduler. Here the accept      */
+;*    is not executed withing the threads but inside the main          */
+;*    loop which tries to accept as many as possible connections       */
+;*    at a time.                                                       */
+;*    -------------------------------------------------------------    */
+;*    See the accept implementation to understand the difference.      */
+;*=====================================================================*/
+
+;*---------------------------------------------------------------------*/
+;*    The module                                                       */
+;*---------------------------------------------------------------------*/
+(module hopsched_scheduler-accept-many
+
+   (library hop)
+   
+   (import  hopsched_scheduler
+	    hopsched_scheduler-pool)
+
+   (export  (class accept-many-scheduler::pool-scheduler)))
+
+		   
+		   
