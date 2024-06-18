49824719b356c8f22013bd5e136dc8a6c22f6935
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-06-18 11:05:50 +0200

    Fix so cache creation

--- nodejs/syncg.scm
+++ nodejs/syncg.scm
@@ -3,8 +3,8 @@
 ;*    -------------------------------------------------------------    */
 ;*    Author      :  Manuel Serrano                                    */
 ;*    Creation    :  Thu Apr 20 08:04:06 2017                          */
-;*    Last change :  Thu Oct 24 12:03:05 2019 (serrano)                */
-;*    Copyright   :  2017-19 Manuel Serrano                            */
+;*    Last change :  Tue Jun 18 10:59:14 2024 (serrano)                */
+;*    Copyright   :  2017-24 Manuel Serrano                            */
 ;*    -------------------------------------------------------------    */
 ;*    Global inter-process synchronization                             */
 ;*=====================================================================*/
@@ -22,47 +22,19 @@
 ;*    synchronize-process ...                                          */
 ;*---------------------------------------------------------------------*/
 (define (synchronize-global lockfile proc)
-   
-   (define (synchronize-file lockfile proc)
-      (let ((dir (dirname lockfile)))
-	 (unless (directory? dir)
-	    (make-directories dir))
-	 (synchronize syncg-mutex
-	    (when (eq? syncg-state 'init)
-	       (set! syncg-state 'running)
-	       (register-exit-function!
-		  (lambda (status)
-		     (tprint "EXIT GLOBAL...")
-		     (synchronize syncg-mutex
-			(set! syncg-state 'exit)
-			(when (file-exists? lockfile)
-			   (delete-file lockfile)))))))
-	 (let loop ()
-	    (if (file-exists? lockfile)
-		(begin
-		   (sleep 1000)
-		   (loop))
-		(synchronize syncg-mutex
-		   (when (eq? syncg-state 'running)
-		      (unwind-protect
-			 (begin
-			    (let ((p (open-output-file lockfile)))
-			       (display (getpid) p)
-			       (close-output-port p)
-			       (proc)))
-			 (delete-file lockfile))))))))
-   
-   (cond-expand
-      (|bigloo4.3a|
-       (synchronize-file lockfile proc))
-      (else
-       (call-with-output-file lockfile
-	  (lambda (port)
-	     (unwind-protect
-		(begin
-		   (lockf port 'lock)
-		   (proc))
-		(lockf port 'ulock)))))))
+   (let* ((path (if (char=? (string-ref lockfile 0) (file-separator))
+		    lockfile
+		    (make-file-name (pwd) lockfile)))
+	  (dir (dirname path)))
+      (unless (directory? dir)
+	 (make-directories dir))
+      (call-with-output-file path
+	 (lambda (port)
+	    (unwind-protect
+	       (begin
+		  (lockf port 'lock)
+		  (proc))
+	       (lockf port 'ulock))))))
 
 ;*---------------------------------------------------------------------*/
 ;*    global variables                                                 */
